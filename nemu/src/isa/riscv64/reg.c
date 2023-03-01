0c1661e17502837d6b5e983e6f3ce18f06eb9df9
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-01 22:53:42 +0800

    > compile NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:53:42 up  8:36,  1 user,  load average: 0.49, 0.31, 0.28

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -31,6 +31,7 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  printf("%s\n",s);
   for(int i =0;i<32;i++){
     int flag = 1;
     for(int j = 0;j<2;j++){
