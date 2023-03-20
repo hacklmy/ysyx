693fae8feb791e3644d06b1f12ea765f437241c4
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-20 23:20:44 +0800

    > compile NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:20:44 up  3:29,  1 user,  load average: 0.14, 0.17, 0.18

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,11 +5,22 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t len = 0;
+  while(*s!='\0'){
+    len++;
+    s++;
+  }
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *res = dst;
+  while(*src != '\0'){
+    *res = *src;
+    res++;
+    src++;
+  }
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -17,11 +28,23 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *res = dst;
+  res += strlen(res);
+  while((*res++ = *src++) != '\0');
+  *res = '\0';
+  return dst;
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  while(*s1 == *s2){
+    if(*s1 == '\0'){
+      return 0;
+    }
+    s1++;
+    s2++;
+  }
+  if(*s1 < *s2) return -1;
+  else return 1;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -29,7 +52,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  int *res = (int *)s;
+  for(int i = n;i > 0;i--){
+    *res = c;
+    res++;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
@@ -37,7 +65,13 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  char *res = (char *)out;
+  for(int i = n;i > 0 && *(char*)in != '\0';i--){
+    *res = *(char*)in;
+    res++;
+    in++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
