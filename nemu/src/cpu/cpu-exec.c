f1b6c7a425f9d668a8e461a4b24334ade5b103ce
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 14:44:40 +0800

    > compile NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:44:40 up  4:58,  1 user,  load average: 0.24, 0.23, 0.19

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -69,7 +69,11 @@ static void exec_once(Decode *s, vaddr_t pc) {
   for (i = ilen - 1; i >= 0; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
-  iringbuf[iringbuf_pointer] = p;
+  int j = 0;
+  while(*p!='\0'){
+    iringbuf[iringbuf_pointer][j++] = *p;
+    p++;
+  }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
   if (space_len < 0) space_len = 0;
@@ -79,6 +83,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
+  strcat(iringbuf[iringbuf_pointer], p);
   iringbuf_pointer = (iringbuf_pointer+1)%16;
 #endif
 }
