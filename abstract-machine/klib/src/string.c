fe6c2128da13e40c3d467b984aca72c5197ad6db
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-21 10:18:10 +0800

    > compile NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:18:10 up 38 min,  1 user,  load average: 0.16, 0.17, 0.18

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -103,19 +103,7 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  if (s1 == NULL || n < 0 || s2 == NULL)
-    return -1;
-  const char *str_s1 = s1;
-  const char *str_s2 = s2;
-
-  for (int i = 0; i < n; i++)
-  {
-    if (str_s1[i] > str_s2[i])
-      return 1;
-    else if (str_s1[i] < str_s2[i])
-      return -1;
-  }
-  return 0;
+  return strncmp(s1, s2, n);
 }
 
 #endif
