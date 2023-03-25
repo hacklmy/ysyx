4dd37d59880570a9eeb429660497857331a0bf34
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-25 09:37:32 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:37:32 up 3 min,  1 user,  load average: 1.18, 0.71, 0.29

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,23 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <assert.h>
 #include "Vtop.h"
-#include "verilated.h"
 #include "Vtop__Dpi.h"
+#include "Vtop__Syms.h"
+#include "verilated.h"
 #include <verilated_vcd_c.h>
-
-#define MAX_SIM_TIME 5
+#include "Vtop___024unit.h"
+#include "Vtop___024root.h"
+#define MAX_SIM_TIME 20
 vluint64_t sim_time = 0;
-int cpu_stop = 0;
-typedef uint64_t paddr_t;
-#define PG_ALIGN __attribute((aligned(4096)))
-#define CONFIG_MSIZE 0x8000000
-#define CONFIG_MBASE 0x80000000
-static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {0};
-uint8_t* guest_to_host(paddr_t paddr) { return paddr - CONFIG_MBASE + pmem; }
-//paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
+uint32_t inst[] = {
+	0x10400b3,
+	0x10400b3,
+	0x10400b3
+};
 
-static inline uint32_t host_read(void *addr) {
+#define PMEM64 1
+#define CONFIG_MBASE 0x80000000
+#define CONFIG_MSIZE 0x8000000
+#define CONFIG_ISA64 1
+#define CONFIG_RT_CHECK 1
+#define PG_ALIGN __attribute((aligned(4096)))
+typedef uint64_t paddr_t;
+typedef uint64_t word_t;
+
+
+static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {0};
+uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+
+static inline uint32_t host_read(void *addr){
   return *(uint64_t *)addr;
 }
 
@@ -26,72 +39,38 @@ static uint32_t pmem_read(paddr_t addr) {
   return ret;
 }
 
-void ebreak_handle(int flag){
-  cpu_stop = flag;
+int stop = 0;
+void ebreak(int flag) {
+  if(flag)printf("ebreak\n");
+  stop = flag;
 }
 
-void load_img(char *img_file){
-  FILE *fp = fopen(img_file, "rb");
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
-  fseek(fp, 0, SEEK_SET);
-  int ret = fread(guest_to_host(0x80000000), size, 1, fp);
-  assert(ret == 1);
-  fclose(fp);
-}
+void cpu_init();
+void exec_once(VerilatedVcdC* m_trace);
+void cpu_exec(uint64_t n);
+void pmem_init();
 
-int main(int argc, char** argv) {
-  VerilatedContext* contextp = new VerilatedContext;
-  contextp->commandArgs(argc, argv);
-  Vtop* top = new Vtop{contextp};
-  
-  VerilatedVcdC* tfp = new VerilatedVcdC; //初始化VCD对象指针
-  contextp->traceEverOn(true); //打开追踪功能
-  top->trace(tfp, 0);
-  tfp->open("wave.vcd"); //设置输出的文件wave.vcd
-   uint32_t inst[] = {
-     0x00108093,
-     0x00108093,
-     0x00108093,
-     0x00108093,
-     0x00108093,
-     0x00108093,
-     0x00108093,
-     0x00100073
-   };
-  memcpy(guest_to_host(0x80000000), inst, sizeof(inst));
-  //int finish = 3;
-  //load_img("../image.bin");
-  top->reset = 1;
-    int n = 10;
-    while (n-- > 0) {
-      top->clock = 0;
-      top->eval();
-      top->clock = 1;
-      top->eval();
+int main(int argc, char** argv, char** env) {
+  Vtop* dut = new Vtop;
+  Verilated::traceEverOn(true);
+  VerilatedVcdC *m_trace = new VerilatedVcdC;
+  dut->trace(m_trace, 5);
+  m_trace->open("waveform.vcd");
+  memcpy(guest_to_host(CONFIG_MBASE), inst, sizeof(inst));
+	while (sim_time < MAX_SIM_TIME && !stop ) {
+		if(sim_time < 3){
+			dut->reset = 1;
+		}else{
+      dut->reset = 0;
     }
-  top->reset = 0;
-  while (!cpu_stop) {
-    top->io_inst = pmem_read(top->io_pc);
-    printf("%lx %lx %x\n",top->io_pc ,top->io_outval, top->io_inst);
-    top->clock = 0;
-    top->eval();
-    printf("%lx %lx\n",top->io_pc ,top->io_outval);
-    top->clock = 1;
-    top->eval();
-    //printf("%lx %lx\n",top->io_pc ,top->io_outval);
-    // top->clock = 0;
-    // top->eval();
-    // top->clock = 1;
-    // top->eval();
-    tfp->dump(contextp->time()); //dump wave
-    //contextp->timeInc(1); //推动仿真时间
-    sim_time++;
-    //assert(top->f == a ^ b);
-    
-  }
-  delete top;
-  tfp->close();
-  delete contextp;
-  return 0;
+		dut->clock ^= 1;
+    dut->io_inst = pmem_read(dut->io_pc);
+		dut->eval();
+		m_trace->dump(sim_time);
+    printf("pc:%lx, inst:%x, stop:%d\n", dut->io_pc, dut->io_inst,stop);
+		sim_time++;
+	}
+  delete dut;
+  m_trace->close();
+  exit(EXIT_SUCCESS);
 }
