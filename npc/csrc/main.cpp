8ffb735cda6576c1807b4a9df91a50606d24090f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-25 11:21:31 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:21:31 up  1:10,  1 user,  load average: 0.17, 0.20, 0.24

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -31,7 +31,7 @@ void ebreak_handle(int flag){
   cpu_stop = flag;
 }
 
-char* img_file = '/home/ysyx-workbench//dummy-riscv64-npc.bin';
+char* img_file = "/home/ysyx-workbench//dummy-riscv64-npc.bin";
 
 void load_img(){
   FILE *fp = fopen(img_file, "rb");
