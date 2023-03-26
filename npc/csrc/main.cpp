9d81d10802ab5c624f672c6dfc87f7a5584a5d9b
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-26 20:08:16 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:16 up  9:52,  0 users,  load average: 0.33, 0.31, 0.28

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -285,6 +285,7 @@ void cpu_exec(int n){
     //printf("%s\n", p);
     memset(s, ' ', 1);
     s += 1;
+    printf("%s\n",p);
     disassemble(s, p + sizeof(p) - s, top->io_pc, guest_to_host(top->io_pc), 4);
     printf("1\n");
     *(s+1) = '\n';
