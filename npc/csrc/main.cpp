ec90fd1feabd36cfa2cb4e6ae4c7c4c11b07c36d
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-26 19:37:39 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:37:39 up  9:21,  0 users,  load average: 0.30, 0.24, 0.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -162,11 +162,7 @@ int sdb_mainloop() {
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCDisassembler/MCDisassembler.h"
 #include "llvm/MC/MCInstPrinter.h"
-#if LLVM_VERSION_MAJOR >= 14
-#include "llvm/MC/TargetRegistry.h"
-#else
 #include "llvm/Support/TargetRegistry.h"
-#endif
 #include "llvm/Support/TargetSelect.h"
 
 #if defined(__GNUC__) && !defined(__clang__)
