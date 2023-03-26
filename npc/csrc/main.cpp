f4fde0d9dc910b76b11a4ddd28c6533114e81343
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-26 19:14:07 +0800

    > compile NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:14:07 up  8:58,  0 users,  load average: 0.39, 0.23, 0.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -18,6 +18,9 @@ int cpu_stop = 0;
 Vtop* top;
 VerilatedVcdC* tfp;
 VerilatedContext* contextp;
+
+void cpu_exec(int n);
+
 //===========================mem=========================
 typedef uint64_t paddr_t;
 #define PG_ALIGN __attribute((aligned(4096)))
@@ -44,27 +47,6 @@ void ebreak_handle(int flag){
 uint64_t *cpu_gpr = NULL;
 extern "C" void set_gpr_ptr(const svOpenArrayHandle r) {
   cpu_gpr = (uint64_t *)(((VerilatedDpiOpenVar*)r)->datap());
-}
-
-
-void cpu_exec(int n){
-  if(n<0)n=100000000;
-  while (!cpu_stop && n--) {
-    if(sim_time<3){
-      top->reset = 1;
-      top->clock^=1;
-      top->eval();
-    }else{
-      top->reset = 0;
-      top->io_inst = pmem_read(top->io_pc);
-      printf("%lx %x\n",top->io_pc , top->io_inst);
-      top->clock ^= 1;
-      top->eval();
-    }
-    tfp->dump(contextp->time()); //dump wave
-    sim_time++;
-    
-  }
 }
 
 
@@ -170,8 +152,102 @@ int sdb_mainloop() {
   return 1;
 }
 
+//==============================itrace===============================
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+
+#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCContext.h"
+#include "llvm/MC/MCDisassembler/MCDisassembler.h"
+#include "llvm/MC/MCInstPrinter.h"
+#if LLVM_VERSION_MAJOR >= 14
+#include "llvm/MC/TargetRegistry.h"
+#else
+#include "llvm/Support/TargetRegistry.h"
+#endif
+#include "llvm/Support/TargetSelect.h"
+
+#if defined(__GNUC__) && !defined(__clang__)
+#pragma GCC diagnostic pop
+#endif
+
+#if LLVM_VERSION_MAJOR < 11
+#error Please use LLVM with major version >= 11
+#endif
+
+using namespace llvm;
+
+static llvm::MCDisassembler *gDisassembler = nullptr;
+static llvm::MCSubtargetInfo *gSTI = nullptr;
+static llvm::MCInstPrinter *gIP = nullptr;
+
+extern "C" void init_disasm(const char *triple) {
+  llvm::InitializeAllTargetInfos();
+  llvm::InitializeAllTargetMCs();
+  llvm::InitializeAllAsmParsers();
+  llvm::InitializeAllDisassemblers();
+
+  std::string errstr;
+  std::string gTriple(triple);
+
+  llvm::MCInstrInfo *gMII = nullptr;
+  llvm::MCRegisterInfo *gMRI = nullptr;
+  auto target = llvm::TargetRegistry::lookupTarget(gTriple, errstr);
+  if (!target) {
+    llvm::errs() << "Can't find target for " << gTriple << ": " << errstr << "\n";
+    assert(0);
+  }
+
+  MCTargetOptions MCOptions;
+  gSTI = target->createMCSubtargetInfo(gTriple, "", "");
+  std::string isa = target->getName();
+  if (isa == "riscv32" || isa == "riscv64") {
+    gSTI->ApplyFeatureFlag("+m");
+    gSTI->ApplyFeatureFlag("+a");
+    gSTI->ApplyFeatureFlag("+c");
+    gSTI->ApplyFeatureFlag("+f");
+    gSTI->ApplyFeatureFlag("+d");
+  }
+  gMII = target->createMCInstrInfo();
+  gMRI = target->createMCRegInfo(gTriple);
+  auto AsmInfo = target->createMCAsmInfo(*gMRI, gTriple, MCOptions);
+#if LLVM_VERSION_MAJOR >= 13
+   auto llvmTripleTwine = Twine(triple);
+   auto llvmtriple = llvm::Triple(llvmTripleTwine);
+   auto Ctx = new llvm::MCContext(llvmtriple,AsmInfo, gMRI, nullptr);
+#else
+   auto Ctx = new llvm::MCContext(AsmInfo, gMRI, nullptr);
+#endif
+  gDisassembler = target->createMCDisassembler(*gSTI, *Ctx);
+  gIP = target->createMCInstPrinter(llvm::Triple(gTriple),
+      AsmInfo->getAssemblerDialect(), *AsmInfo, *gMII, *gMRI);
+  gIP->setPrintImmHex(true);
+  gIP->setPrintBranchImmAsAddress(true);
+}
+
+extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
+  MCInst inst;
+  llvm::ArrayRef<uint8_t> arr(code, nbyte);
+  uint64_t dummy_size = 0;
+  gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
+
+  std::string s;
+  raw_string_ostream os(s);
+  gIP->printInst(&inst, pc, "", *gSTI, os);
+
+  int skip = s.find_first_not_of('\t');
+  const char *p = s.c_str() + skip;
+  assert((int)s.length() - skip < size);
+  strcpy(str, p);
+}
 
 
+
+//==========================itrace_end==============================
+
+//==========================load_img================================
 void load_img(){
   
   char img_file[] = "/home/lmy/ysyx-workbench/npc/image.bin";
@@ -187,6 +263,35 @@ void load_img(){
   
   fclose(fp);
 }
+//============================load_img_end===========================
+void cpu_exec(int n){
+  if(n<0)n=100000000;
+  while (!cpu_stop && n--) {
+    if(sim_time<3){
+      top->reset = 1;
+      top->clock^=1;
+      top->eval();
+    }else{
+      top->reset = 0;
+      top->io_inst = pmem_read(top->io_pc);
+      printf("%lx %x\n",top->io_pc , top->io_inst);
+#ifdef CONFIG_ITRACE
+    FILE* fp = fopen(home/lmy/ysyx-workbench/npc/npc-log.txt);
+    char p[64];
+    p += snprintf(p, sizeof(s->logbuf), "0x%016lx:", top->io_pc);
+    p += snprintf(p, 4, " %08x", top->io_inst);
+#endif
+      top->clock ^= 1;
+      top->eval();
+      top->clock ^= 1;
+      top->eval();
+    }
+    tfp->dump(contextp->time()); //dump wave
+    sim_time++;
+    
+  }
+}
+
 
 int main(int argc, char** argv) {
   contextp = new VerilatedContext;
