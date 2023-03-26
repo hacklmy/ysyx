899cf23153a926dcb939209092238ae84c606d28
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-26 20:14:55 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:14:55 up  9:59,  0 users,  load average: 0.33, 0.26, 0.26

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -230,6 +230,7 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
+  printf("1\n");
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
   printf("1\n");
   std::string s;
