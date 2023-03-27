854030aac49c258b5cd8e875c358e2140a70db3f
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-27 20:36:44 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:36:44 up  2:16,  0 users,  load average: 0.43, 0.28, 0.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -269,7 +269,7 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
 
 //==========================difftest================================
 enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
-void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_memcpy)(uint32_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
@@ -281,7 +281,7 @@ void init_difftest(char *ref_so_file, long img_size) {
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
-  ref_difftest_memcpy = (void (*)(uint64_t addr, void *buf, size_t n, bool direction))dlsym(handle, "difftest_memcpy");
+  ref_difftest_memcpy = (void (*)(uint32_t addr, void *buf, size_t n, bool direction))dlsym(handle, "difftest_memcpy");
   assert(ref_difftest_memcpy);
 
   ref_difftest_regcpy = (void (*)(void *dut, bool direction))dlsym(handle, "difftest_regcpy");
