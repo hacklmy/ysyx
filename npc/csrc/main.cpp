deac8a5b11504b04066108a22caa3db8bffa5dba
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-28 15:13:44 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:44 up  5:02,  1 user,  load average: 0.60, 0.33, 0.23

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -331,11 +331,11 @@ void init_difftest(char *ref_so_file, long img_size) {
   ref_difftest_raise_intr = (void (*)(uint64_t NO))dlsym(handle, "difftest_raise_intr");
   assert(ref_difftest_raise_intr);
 
-  void (*ref_difftest_init)() = (void (*)())dlsym(handle, "difftest_init");
+  void (*ref_difftest_init)(int) = (void (*)(int port))dlsym(handle, "difftest_init");
   assert(ref_difftest_init);
 
 
-  ref_difftest_init();
+  ref_difftest_init(1);
   ref_difftest_memcpy(CONFIG_MBASE, pmem, img_size, DIFFTEST_TO_REF);
   ref_difftest_regcpy(&cpu_gpr, DIFFTEST_TO_REF);
 }
