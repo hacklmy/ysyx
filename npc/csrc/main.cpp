43f64145ad1e8870b1f4b04a422680bbd977278c
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-28 15:42:16 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:42:16 up  5:27,  1 user,  load average: 0.62, 0.47, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -459,7 +459,7 @@ int main(int argc, char** argv) {
     top->eval();
   }
   char difftest_file[] = "/home/lmy/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so";
-  init_difftest(difftest_file,CONFIG_MSIZE);
+  init_difftest(difftest_file,65536);
   while(sdb_mainloop() && !cpu_stop);
   if(stop_status==0)printf("\33[1;32mHIT GOOD TRAP\n\33[0m");
   else printf("\33[1;31mHIT BAD TRAP\n\33[0m");
