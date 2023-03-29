148a7ac1c5a167632086b87462a14e8d2e133620
tracer-ysyx2204 <tracer@ysyx.org>
2023-03-29 18:47:37 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:47:37 up  8:36,  1 user,  load average: 0.28, 0.21, 0.19

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -35,7 +35,6 @@ uint8_t* new_space(int size) {
 static void check_bound(IOMap *map, paddr_t addr) {
   if (map == NULL) {
     Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
-    printf("1\n");
   } else {
     Assert(addr <= map->high && addr >= map->low,
         "address (" FMT_PADDR ") is out of bound {%s} [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
