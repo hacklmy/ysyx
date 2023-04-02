9bb1b3275e30c3a40283d69d26db0242d7290762
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 14:11:40 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:11:40 up  3:32,  0 users,  load average: 0.13, 0.22, 0.25

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -66,12 +66,11 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  #ifdef CONFIG_DTRACE
-  printf("write device at addr: %x  len: %02d name: %s write_value: %lx\n", addr, len, map->name, data);
-  #endif
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
-  
+  #ifdef CONFIG_DTRACE
+  printf("write device at addr: %x  len: %02d name: %s write_value: %lx\n", addr, len, map->name, data);
+  #endif
 }
