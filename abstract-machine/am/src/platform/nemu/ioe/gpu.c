919cdbb8123b69fcdf474eb05af689f0e351dde6
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 16:59:15 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:15 up  6:19,  1 user,  load average: 0.51, 0.41, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,41 +1,37 @@
 #include <am.h>
 #include <nemu.h>
+#include <stdio.h>
 
-#define W    400  // only support 400*300*32
-#define H    300  // only support 400*300*32
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-
+int width, height;
 void __am_gpu_init() {
   int i;
-  int w = W;
-  int h = H;
+  uint32_t vga_ctl = inl(VGACTL_ADDR);
+  width= (vga_ctl>>16);  // TODO: get the correct width
+  height = (vga_ctl)&((1<<16)-1);  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++){
-    fb[i] = i;
-  }
+  for (i = 0; i < width * height; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = W, .height = H,
-    .vmemsz = W*H*sizeof(uint32_t)
+    .width = width, .height = height,
+    .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t *pixels = (uint32_t*)(ctl->pixels);
-  int x = ctl->x, y = ctl->y;
-  int w = ctl->w, h = ctl->h;
-  for(int j = 0; j< h; j++){
-    for(int i = 0; i< w; i++){
-      fb[(y+j)*W+(x+i)] = *(pixels+j*w+i);
+  int cnt=0;
+  for(int j = 0; j < ctl->h ; j ++){
+    for (int i = 0; i < ctl->w; i ++){
+      int p = ctl->y + j;
+      int q = ctl->x + i;
+      fb[p * width + q] = ((uint32_t*)ctl->pixels)[++cnt];
     }
   }
-
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
