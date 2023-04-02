e485f8ebc14aeb115ffd2e8756a1d13bb28d7dcb
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-02 15:53:17 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:53:17 up  5:13,  1 user,  load average: 0.37, 0.39, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,20 +5,7 @@
 #define KEY_QUEUE_LEN 1024
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t key = inl(KBD_ADDR);
-  int keycode = key % KEY_QUEUE_LEN;
-  int keydown = key & KEYDOWN_MASK;
-  if (keycode == AM_KEY_NONE)
-  {
-    kbd->keydown = false;
-    kbd->keycode = AM_KEY_NONE;
-  }
-  else
-  {
-    if (keydown != 0)
-      kbd->keydown = true;
-    else
-      kbd->keydown = false;
-    kbd->keycode = keycode;
-  }
+  uint32_t code = inl(KBD_ADDR);
+  kbd->keydown = ((code & KEYDOWN_MASK) != 0);
+  kbd->keycode = ((code | KEYDOWN_MASK) ^ KEYDOWN_MASK);
 }
