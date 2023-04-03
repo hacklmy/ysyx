e99f452c09c5c63f5adaad8a9eb021eb6ed9a34e
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 23:30:59 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:30:59 up  3:47,  1 user,  load average: 0.34, 0.22, 0.19

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -1,5 +1,4 @@
 /* verilator lint_off UNUSED */
-/* verilator lint_off WIDTH */
 module EXU(
   input         clock,
   input  [63:0] io_pc,
@@ -649,5 +648,4 @@ end // initial
 `endif
 `endif // SYNTHESIS
 endmodule
-/* verilator lint_on WIDTH */
 /* verilator lint_on UNUSED */
