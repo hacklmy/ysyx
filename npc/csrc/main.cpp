7b0056fe7748800397f661ff031392de43854af1
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-03 21:40:48 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:48 up  1:57,  1 user,  load average: 0.23, 0.19, 0.22

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -82,6 +82,7 @@ static inline uint32_t host_read(void *addr) {
 extern "C" void pmem_read(long long raddr, long long *rdata) {
   // 总是读取地址为`raddr & ~0x7ull`的8字节返回给`rdata`
   if(raddr==RTC_ADDR){
+    printf("read rtl\n");
     time_t tmpcal_ptr;
     tmpcal_ptr = time(NULL);
     *rdata = tmpcal_ptr;
@@ -100,6 +101,7 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
   // `wmask`中每比特表示`wdata`中1个字节的掩码,
   // 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
   if(waddr==SERIAL_PORT){
+    printf("putchar\n");
     for (int i = 0; i < 8; i++) {
       if (wmask & 0x1) putchar(wdata&0xf);
       wdata >>= 8;
