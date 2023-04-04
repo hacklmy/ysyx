ed3c01d881bed7b88afbfb12d20bdde616d72a52
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 16:20:29 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:20:29 up 2 min,  1 user,  load average: 0.75, 0.38, 0.15

--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -7,7 +7,7 @@ static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; 
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
-# define DEVICE_BASE 0xa0000000
+#define DEVICE_BASE 0xa0000000
 #define MMIO_BASE 0xa0000000
 
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
