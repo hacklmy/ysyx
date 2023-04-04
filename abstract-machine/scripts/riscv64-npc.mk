ed87859cc0ab21d8614a66519aa5f52eedde967a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-04 14:43:15 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:43:15 up  1:05,  1 user,  load average: 0.18, 0.20, 0.23

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -20,9 +20,9 @@ image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
-	@echo + OBJCOPY "->" ../../../npc/image.bin
-	@cp $(IMAGE).bin ../../../npc/image.bin 
-	@cp $(IMAGE).elf ../../../npc/image.elf
+	@echo + OBJCOPY "->" /home/lmy/ysyx-workbench/npc/image.bin
+	@cp $(IMAGE).bin /home/lmy/ysyx-workbench/npc/image.bin 
+	@cp $(IMAGE).elf /home/lmy/ysyx-workbench/npc/image.elf
 
 run: image
 	$(MAKE) -C /home/lmy/ysyx-workbench/npc sim
