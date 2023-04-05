b51b193f9f7a4fcfcb84835f64cb8f184035c98c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-05 18:44:58 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:44:58 up  8:49,  1 user,  load average: 0.19, 0.16, 0.18

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -28,24 +28,26 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-char *hbrk = NULL;
+
+char *addr_n = NULL;
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-#endif
-  if(hbrk == NULL){
-    hbrk = (void *)ROUNDUP(heap.start, 8);
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//   panic("Not implemented");
+// #endif
+  if(addr_n == NULL){
+    addr_n = (void *)ROUNDUP(heap.start, 8);
   }
   size  = (size_t)ROUNDUP(size, 8);
-  char *old = hbrk;
-  hbrk += size;
-  assert((uintptr_t)heap.start <= (uintptr_t)hbrk && (uintptr_t)hbrk < (uintptr_t)heap.end);
-  for (uint64_t *p = (uint64_t *)old; p != (uint64_t *)hbrk; p ++) {
+  char *addr_c = addr_n;
+  addr_n += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)addr_n && (uintptr_t)addr_n < (uintptr_t)heap.end);
+  for (uint64_t *p = (uint64_t *)addr_c; p != (uint64_t *)addr_n; p ++) {
     *p = 0;
   }
-  return old;
+  return addr_c;
 }
 
 void free(void *ptr) {
