222b99d330976c66b41859b3ab2ea6b2204dd1e5
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 14:22:31 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:22:31 up 18 min,  1 user,  load average: 0.18, 0.39, 0.35

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -5,6 +5,9 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
+extern Area heap;
+char *hbrk = NULL;
+
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
@@ -28,12 +31,13 @@ int atoi(const char* nptr) {
   }
   return x;
 }
-char *hbrk = NULL;
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+  // panic("Not implemented");
 #endif
   if(hbrk == NULL){
     hbrk = (void *)ROUNDUP(heap.start, 8);
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,110 +4,123 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
+size_t strlen(const char *s)
+{
   size_t len = 0;
-  while(*s!='\0'){
+  while (s[len])
     len++;
-    s++;
-  }
   return len;
 }
 
-char *strcpy(char *dst, const char *src) {
-  char *res = dst;
-  while(*src != '\0'){
-    *res = *src;
-    res++;
-    src++;
-  }
-  *res = '\0';
+char *strcpy(char *dst, const char *src)
+{
+  for (int i = 0; src[i]; i++)
+    dst[i] = src[i];
+  dst[strlen(src)] = '\0';
   return dst;
 }
 
-char *strncpy(char *dst, const char *src, size_t n) {
-	char* res = dst;
-	size_t i;
-  	for(i = 0;i< n && *src != '0';i++){
-		*dst++ = *src++;
-  	}
-  	for(;i < n;i++){
-		*dst++ = '\0';
-  	}
-	*dst = '\0';
-  	return res;
-}
-
-char *strcat(char *dst, const char *src) {
-  char *res = dst;
-  res += strlen(dst);
-  while((*res++ = *src++) != '\0');
+char *strncpy(char *dst, const char *src, size_t n)
+{
+  for (int i = 0; i < n; i++)
+    dst[i] = src[i];
   return dst;
 }
 
-int strcmp(const char *s1, const char *s2) {
-  while(*s1 == *s2){
-    if(*s1 == '\0'){
-      return 0;
-    }
-    s1++;
-    s2++;
+char *strcat(char *dst, const char *src)
+{
+  size_t dst_len = strlen(dst);
+  for (int i = 0; src[i]; i++)
+    dst[dst_len + i] = src[i];
+  return dst;
+}
+
+int strcmp(const char *s1, const char *s2)
+{
+  int i = 0;
+  for (i = 0; s1[i] && s2[i]; i++)
+  {
+    if (s1[i] < s2[i])
+      return -1;
+    else if (s1[i] > s2[i])
+      return 1;
   }
-  return *s1 - *s2;
+  if (!s1[i] && s2[i])
+    return -1;
+  if (s1[i] && !s2[i])
+    return 1;
+  return 0;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
-	size_t i = n;
-  	while(*s1 == *s2 && i>0){
-    	if(*s1 == '\0'){
-      		return 0;
-		}
-    	s1++;
-    	s2++;
-		i--;
-  	}
-	return *s1 - *s2;
-}
-
-void *memset(void *s, int c, size_t n) {
-  char *res = (char *)s;
-  for(int i = n;i > 0 && *res!='\0';i--){
-    *res++ = c;
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    if (s1[i] > s2[i])
+      return 1;
+    else if (s1[i] < s2[i])
+      return -1;
   }
-  return s;
+  return 0;
 }
 
-void *memmove(void *dst, const void *src, size_t n) {
-	char* res = (char *)dst;
-	char* in = (char *)src;
-	if (dst < src) {
-		while (n--) {
-			*res++ = *in++;
-		}
-	} else {
-		while (n--) {
-			*(res + n) = *(in + n);
-		}
-	}
-	return dst;
+void *memset(void *s, int c, size_t n)
+{
+  if (s == NULL || n < 0)
+    return NULL;
+
+  void *tmp = s;
+  char *str = s;
+  for (int i = 0; i < n; i++)
+    str[i] = c;
+  return tmp;
 }
 
-void *memcpy(void *out, const void *in, size_t n) {
-  if(out==NULL || in==NULL)return NULL;
-  char *res = (char *)out;
-  for(int i = n;i > 0 && *(char*)in != '\0';i--){
-    *res = *(char*)in;
-    res++;
-    in++;
+void *memmove(void *dst, const void *src, size_t n)
+{
+  if (dst == NULL || n < 0 || src == NULL)
+    return NULL;
+
+  char *str_dst = dst;
+  const char *str_src = src;
+  if (str_dst > str_src && str_src + n > str_dst)
+  {
+    for (int i = n - 1; i >= 0; i--)
+      str_dst[i] = str_src[i];
   }
+  else
+  {
+    for (int i = 0; i < n; i++)
+      str_dst[i] = str_src[i];
+  }
+  return dst;
+}
+
+void *memcpy(void *out, const void *in, size_t n)
+{
+  if (out == NULL || n < 0 || in == NULL)
+    return NULL;
+
+  char *str_out = out;
+  const char *str_in = in;
+  for (int i = 0; i < n; i++)
+    str_out[i] = str_in[i];
   return out;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  int cmp_ans;
-  while (n--)
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  if (s1 == NULL || n < 0 || s2 == NULL)
+    return -1;
+  const char *str_s1 = s1;
+  const char *str_s2 = s2;
+
+  for (int i = 0; i < n; i++)
   {
-	cmp_ans = *(char*)s1++ - *(char*)s2++;
-	if(cmp_ans!=0)return cmp_ans;
+    if (str_s1[i] > str_s2[i])
+      return 1;
+    else if (str_s1[i] < str_s2[i])
+      return -1;
   }
   return 0;
 }
