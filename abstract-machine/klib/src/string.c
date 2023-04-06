53aa57aacb42592eb78454a41c1e6c215c2512c8
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-06 14:29:07 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:29:07 up 25 min,  1 user,  load average: 0.36, 0.46, 0.41

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -68,63 +68,46 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 	return *s1 - *s2;
 }
 
-void *memset(void *s, int c, size_t n)
-{
-  if (s == NULL || n < 0)
-    return NULL;
-
-  void *tmp = s;
-  char *str = s;
-  for (int i = 0; i < n; i++)
-    str[i] = c;
-  return tmp;
+void *memset(void *s, int c, size_t n) {
+  char *res = (char *)s;
+  for(int i = n;i > 0 && *res!='\0';i--){
+    *res++ = c;
+  }
+  return s;
 }
 
-void *memmove(void *dst, const void *src, size_t n)
-{
-  if (dst == NULL || n < 0 || src == NULL)
-    return NULL;
-
-  char *str_dst = dst;
-  const char *str_src = src;
-  if (str_dst > str_src && str_src + n > str_dst)
-  {
-    for (int i = n - 1; i >= 0; i--)
-      str_dst[i] = str_src[i];
-  }
-  else
-  {
-    for (int i = 0; i < n; i++)
-      str_dst[i] = str_src[i];
-  }
-  return dst;
+void *memmove(void *dst, const void *src, size_t n) {
+	char* res = (char *)dst;
+	char* in = (char *)src;
+	if (dst < src) {
+		while (n--) {
+			*res++ = *in++;
+		}
+	} else {
+		while (n--) {
+			*(res + n) = *(in + n);
+		}
+	}
+	return dst;
 }
 
-void *memcpy(void *out, const void *in, size_t n)
-{
-  if (out == NULL || n < 0 || in == NULL)
-    return NULL;
-
-  char *str_out = out;
-  const char *str_in = in;
-  for (int i = 0; i < n; i++)
-    str_out[i] = str_in[i];
+void *memcpy(void *out, const void *in, size_t n) {
+  if(out==NULL || in==NULL)return NULL;
+  char *res = (char *)out;
+  for(int i = n;i > 0 && *(char*)in != '\0';i--){
+    *res = *(char*)in;
+    res++;
+    in++;
+  }
   return out;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n)
-{
-  if (s1 == NULL || n < 0 || s2 == NULL)
-    return -1;
-  const char *str_s1 = s1;
-  const char *str_s2 = s2;
-
-  for (int i = 0; i < n; i++)
+int memcmp(const void *s1, const void *s2, size_t n) {
+  int cmp_ans;
+  while (n--)
   {
-    if (str_s1[i] > str_s2[i])
-      return 1;
-    else if (str_s1[i] < str_s2[i])
-      return -1;
+	cmp_ans = *(char*)s1++ - *(char*)s2++;
+	if(cmp_ans!=0)return cmp_ans;
   }
   return 0;
 }
