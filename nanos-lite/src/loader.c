9c1ac1af8b387fa109a517f1287ee795a6bd4b02
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 15:02:38 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:38 up  3:52,  1 user,  load average: 0.39, 0.33, 0.26

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,8 +1,7 @@
 #include <proc.h>
+#include <elf.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <elf.h>
-
 
 #if defined(__ISA_AM_NATIVE__)
 #define EXPECT_TYPE EM_X86_64
@@ -13,36 +12,45 @@
 #endif
 
 #ifdef __LP64__
-# define Elf_Ehdr Elf64_Ehdr
-# define Elf_Phdr Elf64_Phdr
+#define Elf_Ehdr Elf64_Ehdr
+#define Elf_Phdr Elf64_Phdr
 #else
-# define Elf_Ehdr Elf32_Ehdr
-# define Elf_Phdr Elf32_Phdr
+#define Elf_Ehdr Elf32_Ehdr
+#define Elf_Phdr Elf32_Phdr
 #endif
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern uint8_t ramdisk_start;
 extern uint8_t ramdisk_end;
 #define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  char buf[RAMDISK_SIZE + 3];
-  ramdisk_read(buf, 0, RAMDISK_SIZE);
-  Elf_Ehdr* ehdr = (Elf_Ehdr*)buf;
+
+static uintptr_t loader(PCB *pcb, const char *filename)
+{
+  unsigned char buffer[RAMDISK_SIZE + 5];
+  ramdisk_read(buffer, 0, RAMDISK_SIZE);
+
+  Elf_Ehdr *ehdr = (Elf_Ehdr *)buffer;
+  Elf_Phdr *phdr = (Elf64_Phdr *)(buffer + ehdr->e_phoff);
   assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
   assert(ehdr->e_machine == EXPECT_TYPE);
-  Elf_Phdr* phdr = (Elf_Phdr*)(buf + ehdr->e_phoff);
-  for(int i = 0; i < ehdr->e_phnum; i++){
-    if(phdr[i].p_type==PT_LOAD){
-      memcpy((uint8_t*)phdr[i].p_vaddr, buf+phdr[i].p_offset, phdr[i].p_filesz);
-      memset((uint8_t*)(phdr[i].p_paddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  uint16_t num = ehdr->e_phnum;
+  unsigned char *mem_pos;
+  for (uint16_t i = 0; i < num; i++)
+  {
+    if (phdr[i].p_type == PT_LOAD)
+    {
+      mem_pos = (unsigned char *)phdr[i].p_paddr;
+      memcpy(mem_pos, buffer + phdr[i].p_offset, phdr[i].p_filesz);
+      mem_pos = (unsigned char *)(phdr[i].p_paddr + phdr[i].p_filesz);
+      memset(mem_pos, 0, phdr[i].p_memsz - phdr[i].p_filesz);
     }
   }
   return ehdr->e_entry;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename)
+{
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", (void *)entry);
-  ((void(*)())entry) ();
+  Log("Jump to entry = 0x%lx", entry);
+  ((void (*)())entry)();
 }
-
