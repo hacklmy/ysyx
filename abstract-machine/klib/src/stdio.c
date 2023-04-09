0b03be07fd727e229a3ce97286306bf165579ee6
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 23:51:20 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:51:20 up  1:34,  1 user,  load average: 0.16, 0.19, 0.23

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,129 +4,80 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-char number[17] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-void print_sign(char *out, int *cnt, long long num_ll, int base)
-{
-  int num_b[30] = {0};
-  int num_b_cnt = 0;
-  if (num_ll == 0)
-    num_b[++num_b_cnt] = 0;
-  else if (num_ll < 0)
-    out[(*cnt)++] = '-', num_ll = -num_ll;
-  while (num_ll != 0)
-    num_b[++num_b_cnt] = num_ll % base, num_ll /= base;
-  for (int i = num_b_cnt; i >= 1; i--)
-    out[(*cnt)++] = number[num_b[i]];
-  return;
+
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  char output[10000];
+  int size = vsprintf(output, fmt, ap);
+  va_end(ap);
+  putstr(output);
+  return size;
 }
 
-void print_unsign(char *out, int *cnt, uint64_t num_ll, int base)
-{
-  int num_b[30] = {0};
-  int num_b_cnt = 0;
-  if (num_ll == 0)
-    num_b[++num_b_cnt] = 0;
-  while (num_ll != 0)
-    num_b[++num_b_cnt] = num_ll % base, num_ll /= base;
-  for (int i = num_b_cnt; i >= 1; i--)
-    out[(*cnt)++] = number[num_b[i]];
-  return;
-}
-
-int vprintf(const char *fmt, va_list ap)
-{
-  char out[4096];
-  int cnt = vsprintf(out, fmt, ap);
-  putstr(out);
-  return cnt;
-}
-int printf(const char *fmt, ...)
-{
-  va_list args;
-  va_start(args, fmt);
-  int siz = vprintf(fmt, args);
-  va_end(args);
-  return siz;
-}
-int vsprintf(char *out, const char *fmt, va_list ap)
-{
-  int cnt = 0;
-  for (int i = 0; fmt[i]; i++)
-  {
-    if (fmt[i] != '%')
-    {
-      out[cnt++] = fmt[i];
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  int num = 0;
+  while(*fmt!='\0'){
+    if(*fmt!='%'){
+      out[num++] = *fmt++;
       continue;
     }
-    int num = 0;
-    long long num_ll = 0;
-    uint64_t num_ull = 0;
-    char chr;
-    char *str = NULL;
-    switch (fmt[i + 1])
+    int ans_int;
+    char* ans_str;
+    char ans_char;
+    switch (*(fmt+1))
     {
-    case 'l':
-      switch (fmt[i + 2])
-      {
-      case 'd':
-        num_ll = va_arg(ap, long long);
-        print_sign(out, &cnt, num_ll, 10);
+      case 's':
+        ans_str = va_arg(ap, char*);
+        while(*ans_str!='\0'){
+          out[num++] = *ans_str++;
+        }
         break;
-      case 'x':
-        num_ll = va_arg(ap, long long);
-        print_sign(out, &cnt, num_ll, 16);
+      case 'd':
+        ans_int = va_arg(ap, int);
+        int num_array[50];
+        int array_len = 0;
+        if(ans_int<0){
+          out[num++] = '-';
+          num = -num;
+        }
+        else if(ans_int==0){
+          num_array[array_len++] = 0;
+        }
+        while(ans_int){
+          num_array[array_len++] = ans_int%10;
+          ans_int/=10;
+        }
+        while(--array_len>=0){
+          out[num++] = num_array[array_len] + '0';
+        }
+        break;
+      case 'c':
+        ans_char = va_arg(ap, int);
+        out[num++] = ans_char;
         break;
       default:
         break;
-      }
-      i++;
-      break;
-    case 'd':
-      num = va_arg(ap, int);
-      print_sign(out, &cnt, num, 10);
-      break;
-    case 'x':
-      num = va_arg(ap, int);
-      print_sign(out, &cnt, num, 16);
-      break;
-    case 'p':
-      num_ull = va_arg(ap, uint64_t);
-      print_unsign(out, &cnt, num_ull, 16);
-      break;
-    case 's':
-      str = va_arg(ap, char *);
-      for (int i = 0; str[i]; i++)
-        out[cnt++] = str[i];
-      break;
-    case 'c':
-      chr = va_arg(ap, int);
-      out[cnt++] = chr;
-      break;
-    default:
-      break;
     }
-    i++;
+    fmt+=2;
   }
-  out[cnt++] = '\0';
-  return cnt;
+  out[num] = '\0';
+  return num;
 }
 
-int sprintf(char *out, const char *fmt, ...)
-{
-  va_list args;
-  va_start(args, fmt);
-  int siz = vsprintf(out, fmt, args);
-  va_end(args);
-  return siz;
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  int num = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return num;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...)
-{
+int snprintf(char *out, size_t n, const char *fmt, ...) {
   panic("Not implemented");
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
-{
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
