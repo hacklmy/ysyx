bf7a61aade752205d43e5c765cc64ed5d49650cd
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 19:26:29 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:26:29 up  1:10,  0 users,  load average: 0.30, 0.23, 0.19

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,14 +1,13 @@
 #include <common.h>
 #include "syscall.h"
-#define STRACE
-void do_syscall(Context *c)
-{
+//#define STRACE
+void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
- #ifdef STRACE
+  #ifdef STRACE
     switch (a[0]) {
     case SYS_exit: printf("sys_exit a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]); break;
     case SYS_yield: printf("sys_yield a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]); break;
@@ -32,34 +31,36 @@ void do_syscall(Context *c)
     case SYS_gettimeofday:printf("sys_gettimeofday a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
   }
   #endif
-  switch (a[0])
-  {
-  case (SYS_yield):
-    yield();
-    c->GPRx = 0;
-    break;
-  case (SYS_exit):
-    halt(a[1]);
-    break;
-  case (SYS_write):
-    if (a[1] == 1 || a[1] == 2)
-    {
-      char *buf = (char *)a[2];
-      int count = a[3];
-      int sum = 0;
-      for (int i = 0; i < count; i++)
-      {
-        sum++;
-        printf("%c", buf[i]);
+  switch (a[0]) {
+    case SYS_exit: halt(a[1]); break;
+    case SYS_yield: yield(); c->GPRx = 0; break;
+    case SYS_open:break;
+    case SYS_read:break;
+    case SYS_write:
+      if(a[1]==1||a[1]==2){
+        char* buf = (char*)a[2];
+        for(int i =0;i<a[3];i++){
+          putch(*(buf+i));
+        }
+        c->GPRx = a[3];
       }
-      c->GPRx = sum;
-    }
-    break;
-  case (SYS_brk):
-    c->GPRx = 0;
-    break;
-  
-  default:
-    panic("Unhandled syscall ID = %d", a[0]);
+      break;
+    case SYS_kill:break;
+    case SYS_getpid:break;
+    case SYS_close:break;
+    case SYS_lseek:break;
+    case SYS_brk:c->GPRx = 0;  break;
+    case SYS_fstat:break;
+    case SYS_time:break;
+    case SYS_signal:break;
+    case SYS_execve:break;
+    case SYS_fork:break;
+    case SYS_link:break;
+    case SYS_unlink:break;
+    case SYS_wait:break;
+    case SYS_times:break;
+    case SYS_gettimeofday:break;
+    default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  
 }
