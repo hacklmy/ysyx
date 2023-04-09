434ba8babdb96a753cca1939d45bcad31a61261c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 15:59:32 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:59:32 up  4:49,  1 user,  load average: 0.45, 0.31, 0.34

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -6,8 +6,9 @@ static Context* do_event(Event e, Context* c) {
       printf("yeild call\n");
       break;
     case EVENT_SYSCALL:
+    printf("%d\n",c->gpr[17]);
       do_syscall(c);
-      printf("%d\n",c->gpr[17]);
+      
       printf("syscall\n");
       break;
     default: panic("Unhandled event ID = %d", e.event);
