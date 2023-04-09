8483c39a5d920114a72db09aa6ec72b350340efb
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 16:34:58 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:34:58 up  5:24,  1 user,  load average: 0.47, 0.32, 0.29

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -7,29 +7,6 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  switch (a[0]) {
-    case SYS_exit: halt(a[1]); break;
-    case SYS_yield: yield(); c->GPRx = 0; break;
-    case SYS_open:break;
-    case SYS_read:break;
-    case SYS_write:break;
-    case SYS_kill:break;
-    case SYS_getpid:break;
-    case SYS_close:break;
-    case SYS_lseek:break;
-    case SYS_brk:break;
-    case SYS_fstat:break;
-    case SYS_time:break;
-    case SYS_signal:break;
-    case SYS_execve:break;
-    case SYS_fork:break;
-    case SYS_link:break;
-    case SYS_unlink:break;
-    case SYS_wait:break;
-    case SYS_times:break;
-    case SYS_gettimeofday:break;
-    default: panic("Unhandled syscall ID = %d", a[0]);
-  }
   #ifdef STRACE
     switch (a[0]) {
     case SYS_exit: printf("sys_exit a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]); break;
@@ -54,4 +31,28 @@ void do_syscall(Context *c) {
     case SYS_gettimeofday:break;
   }
   #endif
+  switch (a[0]) {
+    case SYS_exit: halt(a[1]); break;
+    case SYS_yield: yield(); c->GPRx = 0; break;
+    case SYS_open:break;
+    case SYS_read:break;
+    case SYS_write:break;
+    case SYS_kill:break;
+    case SYS_getpid:break;
+    case SYS_close:break;
+    case SYS_lseek:break;
+    case SYS_brk:break;
+    case SYS_fstat:break;
+    case SYS_time:break;
+    case SYS_signal:break;
+    case SYS_execve:break;
+    case SYS_fork:break;
+    case SYS_link:break;
+    case SYS_unlink:break;
+    case SYS_wait:break;
+    case SYS_times:break;
+    case SYS_gettimeofday:break;
+    default: panic("Unhandled syscall ID = %d", a[0]);
+  }
+  
 }
