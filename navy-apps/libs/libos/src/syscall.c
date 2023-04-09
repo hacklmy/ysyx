8045b8e7f53a01ab31f3ae0be5a675c7ff09cbe0
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 21:41:41 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:41:41 up  3:25,  0 users,  load average: 0.21, 0.19, 0.18

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -65,10 +65,19 @@ int _write(int fd, void *buf, size_t count) {
   return _syscall_(SYS_write, fd, (intptr_t)buf, count);
 }
 
+extern char end;
+intptr_t program_break = (intptr_t)&end;
 void *_sbrk(intptr_t increment) {
-  return (void *)-1;
+  intptr_t old_probreak = program_break;
+  intptr_t new_probreak = program_break + increment;
+  if(!_syscall_(SYS_brk, new_probreak, 0, 0)){
+    program_break = new_probreak;
+    return (void *)old_probreak;
+  }
+  else{
+    return (void *)-1;
+  }
 }
-
 int _read(int fd, void *buf, size_t count) {
   _exit(SYS_read);
   return 0;
