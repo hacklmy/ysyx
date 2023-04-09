8bce8f977a5b9c3433e74ff9f6cc6573039d6f2c
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 22:11:00 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:11:00 up  3:54,  0 users,  load average: 0.20, 0.32, 0.27

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,11 +3,11 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      printf("yeild call\n");
+      //printf("yeild call\n");
       c->mepc +=4;
       break;
     case EVENT_SYSCALL:
-      printf("syscall\n");
+      //printf("syscall\n");
       do_syscall(c);
       c->mepc +=4;
       break;
