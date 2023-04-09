a6df9d18eca778a99e7787791f2193e66a6dc7ca
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 18:03:23 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:03:23 up  6:52,  1 user,  load average: 0.31, 0.32, 0.28

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,24 +11,24 @@ void do_syscall(Context *c) {
     switch (a[0]) {
     case SYS_exit: printf("sys_exit a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]); break;
     case SYS_yield: printf("sys_yield a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]); break;
-    case SYS_open:break;
-    case SYS_read:break;
+    case SYS_open:printf("sys_open a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_read:printf("sys_read a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
     case SYS_write:printf("sys_write a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
-    case SYS_kill:break;
-    case SYS_getpid:break;
-    case SYS_close:break;
-    case SYS_lseek:break;
+    case SYS_kill:printf("sys_kill a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_getpid:printf("sys_getpid a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_close:printf("sys_close a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_lseek:printf("sys_lseek a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
     case SYS_brk:printf("sys_brk a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
-    case SYS_fstat:break;
-    case SYS_time:break;
-    case SYS_signal:break;
-    case SYS_execve:break;
-    case SYS_fork:break;
-    case SYS_link:break;
-    case SYS_unlink:break;
-    case SYS_wait:break;
-    case SYS_times:break;
-    case SYS_gettimeofday:break;
+    case SYS_fstat:printf("sys_fstat a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_time:printf("sys_time a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_signal:printf("sys_signal a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_execve:printf("sys_execve a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_fork:printf("sys_fork a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_link:printf("sys_link a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_unlink:printf("sys_unlink a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_wait:printf("sys_wait a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_times:printf("sys_times a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
+    case SYS_gettimeofday:printf("sys_gettimeofday a0=%d a1=%d a2=%d\n", a[0], a[1], a[2]);break;
   }
   #endif
   switch (a[0]) {
