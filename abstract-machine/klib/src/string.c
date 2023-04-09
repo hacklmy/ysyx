9ec26e57e7e850709012f2b1fb98a5405a6b9244
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-09 23:24:33 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:24:33 up  1:08,  1 user,  load average: 0.29, 0.30, 0.27

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,112 +4,117 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s) {
-  size_t len = 0;
-  while(*s!='\0'){
-    len++;
-    s++;
-  }
-  return len;
+size_t strnlen(const char *s, size_t count)
+{
+	const char *sc;
+	for (sc = s; count-- && *sc != '\0'; ++sc)
+		/* nothing */;
+	return sc - s;
 }
 
-char *strcpy(char *dst, const char *src) {
-  char *res = dst;
-  while(*src != '\0'){
-    *res = *src;
-    res++;
-    src++;
+size_t strlen(const char *s) {
+  size_t count = 0; 
+  for(;*s!='\0'; s++){
+    count++;
   }
-  *res = '\0';
-  return dst;
+  return count;
+}
+
+char *strcpy(char *dst, const char *src) { //test done.
+  char *ret = dst;
+  for(; *src!='\0'; src++,dst++){
+    *dst = *src;
+  }
+  *dst = '\0';
+  return ret;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-	char* res = dst;
-	size_t i;
-  	for(i = 0;i< n && *src != '0';i++){
-		*res++ = *src++;
-  	}
-  	for(;i < n;i++){
-		*res++ = '\0';
-  	}
-	*res = '\0';
-  	return dst;
-}
-
-char *strcat(char *dst, const char *src) {
-  char *res = dst;
-  res += strlen(dst);
-  while((*res++ = *src++) != '\0');
-  return dst;
-}
-
-int strcmp(const char *s1, const char *s2) {
-  while(*s1 == *s2){
-    if(*s1 == '\0'){
-      return 0;
-    }
-    s1++;
-    s2++;
+  char *ret = dst;
+  for(size_t i=0; i<n;i++){
+    *(dst+i) = *(src+i);
   }
-  return *s1 - *s2;
+  *(dst+n) = '\0';
+  return ret;
+}
+
+char *strcat(char *dst, const char *src) {  //test done.
+  char *ret = dst;
+  for(; *dst!='\0'; dst++); // dst -> '\0'
+  strcpy(dst,src);
+  return ret;
+}
+
+int strcmp(const char *s1, const char *s2) {  //test done.
+  int ret = 0;
+  for(; *s1 !='\0' && *s2!='\0' ; s1++,s2++){
+    ret = *s1 - *s2;
+    if(ret!=0)
+      return ret; //can not be break!
+  }
+  ret = *s1 - *s2; //check for finish '\0'.
+  return ret;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-	size_t i = n;
-  	while(*s1 == *s2 && i>0){
-    	if(*s1 == '\0'){
-      		return 0;
-		}
-    	s1++;
-    	s2++;
-		i--;
-  	}
-	return *s1 - *s2;
+  int ret = 0;
+  for(int i=0; i<n ;i++){
+    ret = *(s1+i) - *(s2+i);
+    if(ret!=0)
+      break;
+  }
+  return ret; 
 }
 
-void *memset(void *s, int c, size_t n) {
-  char *res = (char *)s;
-  for(int i = n;i > 0;i--){
-    *res++ = c;
+void *memset(void *s, int c, size_t n) { //test done.
+  unsigned char *p = (unsigned char *)s;
+  for(size_t i=0;i<n;i++){
+    *(p+i) = c;
   }
   return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-	char* res = (char *)dst;
-	char* in = (char *)src;
-	if (dst < src) {
-		while (n--) {
-			*res++ = *in++;
-		}
-	} else {
-		while (n--) {
-			*(res + n) = *(in + n);
-		}
-	}
-	return dst;
+  // this function can be uesd for overlapping areas.
+  if (dst <= src) {
+    // ----|dst----------|src---------|----------
+    char *pdst = (char *) dst;
+    char *psrc = (char *) src;
+    for(size_t i=0; i<n; i++){
+      *pdst++ = *psrc++;
+    }
+  } 
+  else {
+    // ----|src----------|dst---------|----------
+    char *pdst = (char *) dst + n;
+    char *psrc = (char *) src + n;
+    for(size_t i=0; i<n; i++){
+      *--pdst = *--psrc;
+    }
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  if(out==NULL || in==NULL)return NULL;
-  char *res = (char *)out;
-  for(int i = n;i > 0;i--){
-    *res = *(char*)in;
-    res++;
-    in++;
+  // this function can not be uesd for overlapping areas!
+  char *pout = (char *) out;
+  const char *pin = (char *) in;
+  for(size_t i=0; i<n; i++){
+    *pout++ = *pin++;
   }
   return out;
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  int cmp_ans;
-  while (n--)
-  {
-	cmp_ans = *(char*)s1++ - *(char*)s2++;
-	if(cmp_ans!=0)return cmp_ans;
+int memcmp(const void *s1, const void *s2, size_t n) { //test done.
+  int ret = 0;
+  const unsigned char *p1 = s1;
+  const unsigned char *p2 = s2;
+  for(int i=0; i<n ;i++){
+    ret = *(p1+i) - *(p2+i);
+    if(ret!=0)
+      break;
   }
-  return 0;
+  return ret; 
 }
 
 #endif
