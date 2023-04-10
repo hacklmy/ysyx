ccb2e9323b92ccf43e3a2549688aa7f4f02393a3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 16:15:51 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:15:51 up 54 min,  1 user,  load average: 0.40, 0.24, 0.20

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,6 +80,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
   sprintf(iringbuf[iringbuf_pointer], "%lx : %s", s->pc, p);
+  FILE* log_file = fopen("/home/lmy/ysyx-workbench/nemu/build/log.txt","a+");
+  if(fputs(p, log_file)==EOF)exit(0);
+  fputc('\n', log_file);
   iringbuf_pointer = (iringbuf_pointer+1)%32;
 #endif
 
