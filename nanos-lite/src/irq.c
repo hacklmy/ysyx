c45f18abbaf4e8ecd1c1748ed4bb690b33803978
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 21:22:44 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:44 up  5:00,  1 user,  load average: 0.37, 0.26, 0.24

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,15 +1,16 @@
 #include <common.h>
-
 void do_syscall(Context *c);
-
 static Context* do_event(Event e, Context* c) {
+  //printf("do event\n");
   switch (e.event) {
     case EVENT_YIELD:
-      // printf("[Strace - do_event] EVENT_YIELD.\n");
+      //printf("yeild call\n");
+      //c->mepc +=4;
       break;
     case EVENT_SYSCALL:
-      // printf("[Strace - do_event] EVENT_SYSCALL.\n");
+      //printf("syscall\n");
       do_syscall(c);
+      //c->mepc +=4;
       break;
     default: panic("Unhandled event ID = %d", e.event);
   }
