dcc51e754ba483bc4ebfc6eea74582d3c3fe48cf
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 14:56:57 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:56:57 up 20 min,  1 user,  load average: 0.16, 0.20, 0.18

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    //printf("mcasuse: %d  GPR1:%d\n",c->mcause, c->GPR1);
+    printf("mcasuse: %d  GPR1:%d\n",c->mcause, c->GPR1);
     switch (c->mcause) {
       case -1:ev.event = EVENT_YIELD;break;
       case 1:
--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,14 +1,14 @@
 #include <common.h>
 void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
-  //printf("do event\n");
+  printf("do event\n");
   switch (e.event) {
     case EVENT_YIELD:
-      //printf("yeild call\n");
+      printf("yeild call\n");
       c->mepc +=4;
       break;
     case EVENT_SYSCALL:
-      //printf("syscall\n");
+      printf("syscall\n");
       do_syscall(c);
       c->mepc +=4;
       break;
