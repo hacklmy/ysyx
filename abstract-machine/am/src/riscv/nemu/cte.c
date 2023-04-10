418645529317e871e2a61080906d8527b6adc96b
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 21:23:01 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:01 up  5:00,  1 user,  load average: 0.29, 0.25, 0.23

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,15 +7,20 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+    printf("mcasuse: %d  GPR1:%d\n",c->mcause, c->GPR1);
     switch (c->mcause) {
-      case -1: ev.event = EVENT_YIELD; break;   // yield
-      case  0: ev.event = EVENT_SYSCALL; break; // exit
-      case  1: ev.event = EVENT_SYSCALL; break; // yield
-      case  4: ev.event = EVENT_SYSCALL; break; // write
-      case  9: ev.event = EVENT_SYSCALL; break; // brk
+      case -1:
+        ev.event = EVENT_YIELD;
+        break;
+      case 1:
+      case 0:
+      case 4:
+      case 9:
+        ev.event = EVENT_SYSCALL;
+        break;
+
       default: ev.event = EVENT_ERROR; break;
     }
-
     c = user_handler(ev, c);
     assert(c != NULL);
   }
