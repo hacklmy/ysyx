408d1d09980b89ca87ca7583c81d4209a7c109d6
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-10 10:58:00 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:58:00 up 53 min,  1 user,  load average: 0.21, 0.24, 0.25

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,7 +8,9 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
+      
       case 0xb:
+      printf("mcasuse: %d  GPR1:%d\n",c->mcause, c->GPR1);
         if(c->GPR1==-1){
           ev.event = EVENT_YIELD;
         }else{
