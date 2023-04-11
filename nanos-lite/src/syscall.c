92289e4ef34bff7407f0f209058467463f8125e9
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 10:38:32 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:38:32 up 41 min,  0 users,  load average: 0.26, 0.25, 0.20

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,13 @@
 #include <common.h>
 #include "syscall.h"
 //#define STRACE
+
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -34,22 +41,31 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_exit: halt(a[1]); break;
     case SYS_yield: yield(); c->GPRx = 0; break;
-    case SYS_open:break;
-    case SYS_read:break;
+    case SYS_open:
+      c->GPRx = fs_open((const char*)a[1], a[2], a[3]);
+      break;
+    case SYS_read:
+      c->GPRx = fs_read(a[1], (void*)a[2], a[3]);
+      break;
     case SYS_write:
-      printf("syscall\n");
       if(a[1]==1||a[1]==2){
         char* buf = (char*)a[2];
         for(int i =0;i<a[3];i++){
           putch(*(buf+i));
         }
         c->GPRx = a[3];
-      }else c->GPRx = -1;
+      }else if(a[1]!=0){
+        c->GPRx = fs_write(a[1], (void*)a[2], a[3]);
+      }
       break;
     case SYS_kill:break;
     case SYS_getpid:break;
-    case SYS_close:break;
-    case SYS_lseek:break;
+    case SYS_close:
+      c->GPRx = fs_close(a[1]);
+      break;
+    case SYS_lseek:
+      c->GPRx = fs_lseek(a[1], a[2], a[3]);
+      break;
     case SYS_brk: c->GPRx = 0;  break;
     case SYS_fstat:break;
     case SYS_time:break;
