01b7a81072426f78c6ad2320c1b30e53063c72e0
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 10:24:23 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:24:23 up 26 min,  0 users,  load average: 0.23, 0.22, 0.20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,16 +1,6 @@
 #include <proc.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <elf.h>
-
-
-#if defined(__ISA_AM_NATIVE__)
-#define EXPECT_TYPE EM_X86_64
-#elif defined(__ISA_RISCV64__)
-#define EXPECT_TYPE EM_RISCV
-#else
-#define EXPECT_TYPE EM_NONE
-#endif
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -19,38 +9,53 @@
 # define Elf_Ehdr Elf32_Ehdr
 # define Elf_Phdr Elf32_Phdr
 #endif
-extern uint8_t ramdisk_start;
-extern uint8_t ramdisk_end;
-#define RAMDISK_SIZE ((&ramdisk_end) - (&ramdisk_start))
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_386
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_TYPE EM_MIPS
+#elif defined(__ISA_RISCV32__) || defined(__ISA_RISCV64__)
+# define EXPECT_TYPE EM_RISCV
+#elif
+# error unsupported ISA __ISA__
+#endif
+
 int fs_open(const char *pathname, int flags, int mode);
 size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
 size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  int fd = fs_open(filename,0,0);
-  Elf_Ehdr* ehdr = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
-  fs_read(fd,ehdr, sizeof(Elf_Ehdr));
-  assert(*(uint32_t *)ehdr->e_ident == 0x464c457f);
-  assert(ehdr->e_machine == EXPECT_TYPE);
-  Elf_Phdr* phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr)*ehdr->e_phnum);
-  fs_lseek(fd, ehdr->e_phoff, SEEK_SET);
-  fs_read(fd,phdr, sizeof(Elf_Phdr)*ehdr->e_phnum);
-
-  for(int i = 0; i < ehdr->e_phnum; i++){
-    if(phdr[i].p_type==PT_LOAD){
-      fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
-      fs_read(fd,(char*)phdr[i].p_vaddr, phdr[i].p_filesz);
-      memset((char*)(phdr[i].p_paddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
-    }
+  // Log("[Loader] ELF file is reading from '%s'.", filename);
+  int fd = fs_open(filename, 0, 0);
+  // ELF Header
+  Elf_Ehdr *elf = (Elf_Ehdr*)malloc(sizeof(Elf_Ehdr));
+  fs_read(fd, elf, sizeof(Elf_Ehdr));
+  // Check ELF magic number
+  Log("elf->e_ident = 0x%p", *(uint32_t *)elf->e_ident);
+  assert(*(uint32_t *)elf->e_ident == 0x464C457F);
+  // Checl ELF machine
+  Log("elf->e_machine = 0x%p", elf->e_machine);
+  assert(EXPECT_TYPE == elf->e_machine);
+  // Program Header
+  Elf_Phdr *phdr = (Elf_Phdr*)malloc(sizeof(Elf_Phdr) * elf->e_phnum);
+  fs_lseek(fd, elf->e_phoff, SEEK_SET);
+  fs_read(fd, phdr, sizeof(Elf_Phdr) * elf->e_phnum);
+  // Analysis of program header table
+  for (int i = 0; i < elf->e_phnum; ++i) {
+    if (phdr[i].p_type != PT_LOAD) continue;
+    fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+    fs_read(fd, (char*)phdr[i].p_vaddr, phdr[i].p_filesz);
+    memset((char*)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
   }
-  return ehdr->e_entry;
+  return elf->e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", (void *)entry);
+  Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
-
