a094bf712ea6f71851e6da4d6a333af43c01f98a
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 21:40:58 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:58 up  1:27,  1 user,  load average: 0.41, 0.31, 0.29

--- navy-apps/tests/timer-test/main.cpp
+++ navy-apps/tests/timer-test/main.cpp
@@ -13,7 +13,7 @@ int main() {
     while(1){
       gettimeofday(&tv,&tz);
       printf("gettimeofday\n");
-      if(tv->tv_usec/500000>=sec)break;
+      if(tv.tv_usec/500000>=sec)break;
     }
 
     
