624fae642949e6ce4ce3eb43e53879af53297b53
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 11:11:53 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:11:53 up  1:14,  0 users,  load average: 0.44, 0.27, 0.20

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -59,7 +59,7 @@ void do_syscall(Context *c) {
   }
   #ifdef STRACE
     switch (a[0]) {
-    case SYS_exit: printf("sys_exit\n"); break;
+    //case SYS_exit: printf("sys_exit\n"); break;
     case SYS_yield: printf("sys_yield a1=%d\n", a[1]); break;
     case SYS_open:printf("sys_open name=%d\n",get_filename(c->GPRx));break;
     case SYS_read:printf("sys_read name=%s read_len=%d\n",  get_filename(a[1]), a[3]);break;
