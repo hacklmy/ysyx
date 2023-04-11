1917bc5e6ff86c5a2ec2fe750ec4397ddeeeddfc
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-11 10:23:53 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:23:53 up 26 min,  0 users,  load average: 0.19, 0.21, 0.20

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -7,7 +7,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
-  size_t file_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -23,70 +23,69 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
 }
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0,0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0,0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, 0,invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin",  0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
-static int filenum = sizeof(file_table)/sizeof(Finfo);
 
-int fs_open(const char *pathname, int flags, int mode){
-  for(int i =0;i< filenum;i++){
-    if(strcmp(pathname, file_table[i].name)==0){
-      file_table[i].file_offset = 0;
-      return i;
-    }
-  }
-  Log("no match file\n");
-  assert(0);
-}
-size_t fs_read(int fd, void *buf, size_t len){
-  if(file_table[fd].file_offset + len > file_table[fd].size){
-    len = file_table[fd].size - file_table[fd].file_offset;
-    Log("len is out of limit\n");
-  }
-  size_t ret = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].file_offset, len);
-  file_table[fd].file_offset += len;
-  return ret;
-}
-
-size_t fs_write(int fd, const void *buf, size_t len){
-  if(file_table[fd].file_offset + len > file_table[fd].size){
-    len = file_table[fd].size - file_table[fd].file_offset;
-    Log("len is out of limit\n");
-  }
-  size_t ret = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].file_offset, len);
-  file_table[fd].file_offset += len;
-  return ret;
-}
-
-size_t fs_lseek(int fd, size_t offset, int whence){
-  switch(whence){
-    case SEEK_SET:
-      file_table[fd].file_offset = offset;
-      break;
-    case SEEK_CUR:
-      file_table[fd].file_offset += offset;
-      break;
-    case SEEK_END:
-      file_table[fd].file_offset = file_table[fd].size + offset;
-      break;
-    default:
-      Log("wrong file position\n");
-      assert(0);
-  }
-  return file_table[fd].file_offset;
-}
-
-int fs_close(int fd){
-  file_table[fd].file_offset = 0;
-  return 0;
-}
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t get_ramdisk_size();
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  int num = sizeof(file_table) / sizeof(Finfo);
+  for (int i = 0; i < num; ++i) {
+    if (strcmp(pathname, file_table[i].name) == 0) return i;
+  }
+  panic("wrong file pathname.");
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  if (file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+    Log("Warning: fs_read offset has been reset.");
+  }
+  size_t ret = ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+  return ret;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  if (file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+    Log("Warning: fs_write offset has been reset.");
+  }
+  size_t ret = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+  return ret;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  switch (whence) {
+    case SEEK_SET:
+      file_table[fd].open_offset = offset;
+      break;
+    case SEEK_CUR:
+      file_table[fd].open_offset += offset;
+      break;
+    case SEEK_END:
+      file_table[fd].open_offset = file_table[fd].size + offset;
+      break;
+    default:
+      panic("error with fs_lseek");
+  }
+  return file_table[fd].open_offset;
+}
+
+int fs_close(int fd) {
+  file_table[fd].open_offset = 0;
+  return 0;
 }
