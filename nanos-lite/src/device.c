2fa503047b41474bfb5eb3f17b99432925040966
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 11:14:40 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:14:40 up  1:22,  1 user,  load average: 0.23, 0.22, 0.22

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,18 +23,17 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T kbd = io_read(AM_INPUT_KEYBRD);
-  int length;
   if(kbd.keycode==AM_KEY_NONE){
     //printf("no key\n");
     return 0;
   }
   //printf("has a key\n");
   if(kbd.keydown==0){
-    length = snprintf((char *)buf,len,"ku %s\n",keyname[kbd.keycode]);
+    sprintf((char *)buf,"ku %s\n",keyname[kbd.keycode]);
   }else{
-    length = snprintf((char *)buf,len,"kd %s\n",keyname[kbd.keycode]);
+    sprintf((char *)buf,"kd %s\n",keyname[kbd.keycode]);
   }
-  return length;
+  return len;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
