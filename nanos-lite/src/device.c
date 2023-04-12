ca63b831375f2bf562e1f3c5c0f1b13087ab57d3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 14:25:00 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:25:00 up 23 min,  0 users,  load average: 0.30, 0.25, 0.20

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -38,7 +38,8 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
+  return  snprintf((char*)buf, len, "%d %d\n", cfg.width, cfg.height);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -12,7 +12,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR,DEV_EVENT, FD_FB };
+enum {FD_STDIN, FD_STDOUT, FD_STDERR,DEV_EVENT,FD_DISPINFO, FD_FB };
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -26,6 +26,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 size_t events_read(void *buf, size_t offset, size_t len);
+size_t dispinfo_read(void *buf, size_t offset, size_t len);
 
 size_t serial_write(const void *buf, size_t offset, size_t len);
 /* This is the information about all files in disk. */
@@ -34,6 +35,7 @@ static Finfo file_table[] __attribute__((used)) = {
   [FD_STDOUT] = {"stdout", 0, 0,0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, 0,invalid_read, serial_write},
   [DEV_EVENT] = {"/dev/events", 0, 0, 0,events_read, invalid_write},
+  [FD_DISPINFO] = {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
 #include "files.h"
 };
 
