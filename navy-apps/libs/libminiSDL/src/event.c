b22ba4391d8bfc1ff7c4cd83ecb61a71772d5942
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-12 23:11:24 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:24 up  2:40,  1 user,  load average: 0.30, 0.22, 0.20

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,8 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -13,10 +16,29 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
+  char buf[64];
+  if(NDL_PollEvent(buf,sizeof(buf))!=0){
+    if(buf[0]=='k'){
+      if(buf[1]=='d'){
+        ev->type = SDL_KEYDOWN;
+      }else if(buf[1]=='u'){
+        ev->type = SDL_KEYUP;
+      }
+       ev->key.keysym.sym = 0;
+      for(int i =0;i<(sizeof(keyname)/sizeof(keyname[0]));i++){
+        if(strncmp(buf+3,keyname[i], strlen(buf+3)-1)==0){
+          ev->key.keysym.sym = i;
+          break;
+        }
+      }
+      return 1;
+    }
+  }
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  while(SDL_PollEvent(event) == 0);
   return 1;
 }
 
