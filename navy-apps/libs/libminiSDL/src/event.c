e2239bc285f05e009fcf08ae6ec241535efa9793
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 18:54:46 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:54:46 up  3:04,  1 user,  load average: 0.27, 0.27, 0.25

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -11,60 +11,42 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
-#define NUMBER_OF_KEYS 83
-#define EVENT_BUF_LENGTH 64
-char buf[EVENT_BUF_LENGTH];
-uint8_t status[NUMBER_OF_KEYS];
-
 int SDL_PushEvent(SDL_Event *ev) {
-  printf("Unimplemented function: SDL_PushEvent\n");
-  while (1);
   return 0;
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  if (NDL_PollEvent(buf, EVENT_BUF_LENGTH) == 0) {
-    ev->type = SDL_KEYUP;
-    ev->key.keysym.sym = SDLK_NONE;
-    return 0;
-  }
-  if (buf[0] == 'k') {
-    int code = 0;
-    for (int i = 0; i < 83; ++i) {
-      if (strncmp(buf + 3, keyname[i], strlen(buf + 3) - 1) == 0) {
-        code = i;
-        break;
+  char buf[64];
+  if(NDL_PollEvent(buf,sizeof(buf))!=0){
+    if(buf[0]=='k'){
+      if(buf[1]=='d'){
+        ev->type = SDL_KEYDOWN;
+      }else if(buf[1]=='u'){
+        ev->type = SDL_KEYUP;
       }
+       ev->key.keysym.sym = 0;
+       printf("%s\n",buf+3);
+      for(int i =0;i<(sizeof(keyname)/sizeof(keyname[0]));i++){
+        if(strcmp(buf+3,keyname[i])==0){
+          ev->key.keysym.sym = i;
+          break;
+        }
+      }
+      return 1;
     }
-    if (buf[1] == 'd') {
-      ev->type = SDL_KEYDOWN;
-      ev->key.keysym.sym = code;
-      status[code] = 1;
-      // printf("SDL_PollEvent: Key Down %d\n", code);
-    } else if (buf[1] == 'u') {
-      ev->type = SDL_KEYUP;
-      ev->key.keysym.sym = code;
-      status[code] = 0;
-      // printf("SDL_PollEvent: Key Up %d\n", code);
-    }
-    return 1;
   }
-  printf("Should not reach here\n");
-  while (1);
+  return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  // after keydown and keyup, ret should be zero, and then set keysym to NONE.
-  while (SDL_PollEvent(event) == 0);
+  while(SDL_PollEvent(event) == 0);
   return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
-  printf("Unimplemented function: SDL_PeepEvents\n");
-  while (1);
   return 0;
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return status;
+  return NULL;
 }
