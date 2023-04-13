1f0e19a26050a804e3b1c4e53c4cf6f7e86ec7a7
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 19:35:53 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:35:53 up  3:45,  1 user,  load average: 0.16, 0.19, 0.22

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -45,20 +45,27 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-	size_t i = 0;
-	for (; s1[i] != '\0' && s2[i] != '\0'; ++i) {
-		if (s1[i] != s2[i]) return s1[i] - s2[i];
-	}
-	return s1[i] - s2[i];
+  while(*s1 != '\0' && *s2!='\0'){
+    if(*s1 != *s2){
+      return *s1 - *s2;
+    }
+    s1++;
+    s2++;
+  }
+  return *s1 - *s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-	size_t i = 0;
-	for (; s1[i] != '\0' && s2[i] != '\0' && i < n; ++i) {
-		if (s1[i] != s2[i]) return s1[i] - s2[i];
-	}
-	if (i == n) return 0;
-	return s1[i] - s2[i];
+  	while(*s1 != '\0' && *s2!='\0' && n>0){
+    	if(*s1 != *s2){
+      		return *s1 - *s2;
+		  }
+    	s1++;
+    	s2++;
+		  n--;
+  	}
+  if(n==0)return 0;
+	return *s1 - *s2;
 }
 
 void *memset(void *s, int c, size_t n) {
