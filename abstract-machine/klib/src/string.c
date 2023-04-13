5bfda3bbc9257d89ff18eea2d5129a1c9b153fe4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-13 19:15:16 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:15:16 up  3:24,  1 user,  load average: 0.51, 0.27, 0.28

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -45,9 +45,9 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  while(*s1 == *s2){
-    if(*s1 == '\0' || *s2=='\0'){
-      return 0;
+  while(*s1 != '\0' && *s2!='\0'){
+    if(*s1 != *s2){
+      return *s1 - *s2;
     }
     s1++;
     s2++;
