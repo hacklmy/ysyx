eb8fee3d40e115f57400f16f74359f9a445adbb3
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-14 16:01:52 +0800

    > run NEMU ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:01:52 up  1:18,  1 user,  load average: 0.35, 0.26, 0.34

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -43,31 +43,15 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  offset /= 4, len /= 4;
-  // printf("== offset: %d, len: %d ==\n", offset, len);
-  // size of screen
-  AM_GPU_CONFIG_T cfg;
-  ioe_read(AM_GPU_CONFIG, &cfg);
-  // fill
-  AM_GPU_FBDRAW_T ctl;
-  ctl.h = ctl.w = 0;
-  int used = 0;    // number of pixels
-  // (x, y) -> (width, height)
-  ctl.x = offset % cfg.width, ctl.y = offset / cfg.width;
-  // printf("%d %d\n", ctl.x, ctl.y);
-  if (len >= cfg.width) {
-    ctl.h = len / cfg.width, ctl.w = cfg.width;
-    ctl.pixels = (void *)buf;
-    ctl.sync = true;
-    ioe_write(AM_GPU_FBDRAW, &ctl);
-    used += ctl.h * ctl.w * 4;
-  }
-  ctl.x += ctl.h;
-  if (len % cfg.width != 0) {
-    ctl.h = 1, ctl.w = len % cfg.width;
-    ctl.pixels = (void *)(buf + used);
-    ioe_write(AM_GPU_FBDRAW, &ctl);
-  }
+  AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
+  AM_GPU_FBDRAW_T ctl; 
+  ctl.pixels = (void*)buf;
+  ctl.sync = true;
+  ctl.x = offset % cfg.width;
+  ctl.y = offset / cfg.width;
+  ctl.w = ((len >> 32) & 0xffff);
+  ctl.h = (len & 0xffff);
+  ioe_write(AM_GPU_FBDRAW, &ctl);
   return len;
 }
 
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -1,74 +1,91 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <assert.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
-static int canvas_w = 0, canvas_h = 0;
-static int center_w = 0, center_h = 0;
-
 
 uint32_t NDL_GetTicks() {
-  struct timeval tv;
-  struct timezone tz;
-  gettimeofday(&tv, &tz);
-  return tv.tv_usec / 1000 ;
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  return now.tv_usec/1000;
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  int fd = open("/dev/events", 0, 0);
-  int sz = read(fd, buf, len);
-  // if (sz != 0) printf("\"%s\" with %d\n", buf, sz);
-  return sz != 0;
+  int fd = open("/dev/events",0,0);
+  if(fd == -1){
+    return 0;
+  }
+  return read(fd,buf,len);
 }
 
+// 打开一张(*w) X (*h)的画布
+// 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
 void NDL_OpenCanvas(int *w, int *h) {
-  if (getenv("NWM_APP")) {
-    int fbctl = 4;
-    fbdev = 5;
-    screen_w = *w; screen_h = *h;
-    char buf[64];
-    int len = sprintf(buf, "%d %d", screen_w, screen_h);
-    // let NWM resize the window and create the frame buffer
-    write(fbctl, buf, len);
-    while (1) {
-      // 3 = evtdev
-      int nread = read(3, buf, sizeof(buf) - 1);
-      if (nread <= 0) continue;
-      buf[nread] = '\0';
-      if (strcmp(buf, "mmap ok") == 0) break;
-    }
-    close(fbctl);
+
+  int fd = open("/proc/dispinfo",0,0);
+  if(fd == -1){
+    printf("open /proc/dispinfo error");
+    return ;
   }
-  // printf("%d %d\n", screen_w, *w);
-  // printf("%d %d\n", screen_h, *h);
-  if (*w == 0) *w = screen_w;
-  if (*h == 0) *h = screen_h;
-  assert(1 <= *w && *w <= screen_w);
-  assert(1 <= *h && *h <= screen_h);
-  canvas_w = *w;
-  canvas_h = *h;
-  center_w = screen_w / 2 - canvas_w / 2;
-  center_h = screen_h / 2 - canvas_h / 2;
-  // printf("%d %d\n", center_h, center_w);
+
+  char buf[50];
+  read(fd,buf,sizeof(buf));
+  sscanf(buf,"WIDTH:%d\nHEIGHT:%d\n",&screen_w,&screen_h);
+
+  if(*w == 0 && *h == 0){
+    *w = screen_w;
+    *h = screen_h;
+  }
+
+  //printf("WIDTH:%d\nHEIGHT:%d\n",screen_w,screen_h);
+  assert(*w <= screen_w && *h <= screen_h);
+
+  // if (getenv("NWM_APP")) {
+  //   int fbctl = 4;
+  //   fbdev = 5;
+  //   screen_w = *w; screen_h = *h;
+  //   char buf[64];
+  //   int len = sprintf(buf, "%d %d", screen_w, screen_h);
+  //   // let NWM resize the window and create the frame buffer
+  //   write(fbctl, buf, len);
+  //   while (1) {
+  //     // 3 = evtdev
+  //     int nread = read(3, buf, sizeof(buf) - 1);
+  //     if (nread <= 0) continue;
+  //     buf[nread] = '\0';
+  //     if (strcmp(buf, "mmap ok") == 0) break;
+  //   }
+  //   close(fbctl);
+  // }
 }
 
+// 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
+// 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  int fd = open("/dev/fb", 0, 0);
-  // printf("%d\n", fd);
-  for (int i = 0; i < h; ++i) {
-    // printf("write %d\n", i);
-    lseek(fd, ((center_h + y + i) * screen_w + center_w + x) * 4, SEEK_SET);
-    write(fd, pixels + i * w, w * 4);
+  int fd = open("/dev/fb",0,0);
+  if(fd == -1){
+    printf("open /dev/fb error");
+    return ;
   }
+
+// there 2 method to support gpu, check nanos_lite/src/device.c: fb_write() and nanos-lite/src/fs.c:init_fs() to match.
+#if defined(__ISA_NATIVE__) // for native, slow but support native.
+// method 1: only write w for one time, and use loop to finish all.
+  for(int j=0; j<h; j++){
+    lseek(fd,((y+j)*screen_w+x)*4,SEEK_SET); // 4 for 32bits, 4bytes.
+    write(fd, pixels+w*j, 4*w);              // 4 for 32bits, 4bytes.
+  }
+#else // for nemu, fast but not support native.
+// method 2: use high 32bit to store w, low 32bit to store h. 
+  lseek(fd,x*y,SEEK_SET);
+  write(fd, pixels, ((size_t)w<<32) | ((size_t)h & 0x00000000FFFFFFFF)); //w=high 32bit, h=low 32bit.
+#endif
+
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
@@ -89,20 +106,6 @@ int NDL_Init(uint32_t flags) {
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
-  int fd;
-  // fetch size of screen
-  fd = open("/proc/dispinfo", 0, 0);
-  char buf[64];
-  read(fd, buf, 64);
-  int len = strlen(buf), flag = 1;
-  for (int i = 0; i < len; ++i) {
-    if ('0' <= buf[i] && buf[i] <= '9') {
-      if (!flag) screen_w = screen_w * 10 + buf[i] - '0';
-      else screen_h = screen_h * 10 + buf[i] - '0';
-    } else if (buf[i] == ':') flag ^= 1;
-  }
-  // get base ticks
-  
   return 0;
 }
 
