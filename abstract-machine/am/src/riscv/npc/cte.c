73f8e8ebe24a3c46541f93a769ddddadecc9296d
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-16 16:21:08 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:08 up  4:46,  1 user,  load average: 0.44, 0.27, 0.28

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -1,4 +1,5 @@
 #include <am.h>
+#include <riscv/riscv.h>
 #include <klib.h>
 
 static Context* (*user_handler)(Event, Context*) = NULL;
@@ -6,10 +7,31 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
+    //printf("mcasuse: %d  GPR1:%d\n",c->mcause, c->GPR1);
     switch (c->mcause) {
+      case -1:
+        ev.event = EVENT_YIELD;
+        break;
+      case 1:
+      case 0:
+      case 2:
+      case 3:
+      case 8:
+      case 4:
+      case 7:
+      case 9:
+      case 10:
+      case 11:
+      case 12:
+      case 13:
+      case 14:
+      case 15:
+      case 19:
+        ev.event = EVENT_SYSCALL;
+        break;
+
       default: ev.event = EVENT_ERROR; break;
     }
-
     c = user_handler(ev, c);
     assert(c != NULL);
   }
