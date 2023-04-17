01ab41a33636ab036555cda87811940dbba92e43
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-17 14:55:37 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:37 up 15 min,  0 users,  load average: 0.34, 0.28, 0.22

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <klib-macros.h>
+#include <stdio.h>
 
 void __am_timer_init();
 
@@ -24,7 +25,10 @@ static void fail(void *buf) { panic("access nonexist register"); }
 
 bool ioe_init() {
   for (int i = 0; i < LENGTH(lut); i++)
-    if (!lut[i]) lut[i] = fail;
+    if (!lut[i]) {
+      lut[i] = fail;
+      printf("%d\n",i);
+      }
   __am_timer_init();
   return true;
 }
