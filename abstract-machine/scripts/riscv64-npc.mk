1a5f974f477d5f9fc31080c580a7151ada9da2d6
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-18 11:10:48 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:10:48 up  1:03,  1 user,  load average: 0.32, 0.31, 0.24

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -1,6 +1,8 @@
 include $(AM_HOME)/scripts/isa/riscv64.mk
 include $(AM_HOME)/scripts/platform/npc.mk
 
+CFLAGS  += -DISA_H=\"riscv/riscv.h\"
+
 AM_SRCS += riscv/npc/start.S \
            riscv/npc/cte.c \
            riscv/npc/trap.S \
