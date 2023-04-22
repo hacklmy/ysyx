dd821ccbce991831219084dccedfa6524b461983
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 18:05:20 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:05:20 up  7:13,  1 user,  load average: 0.78, 0.54, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -238,6 +238,7 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
   if(waddr >=VGACTL_ADDR && waddr <VGACTL_ADDR+32){
     if(waddr==VGACTL_ADDR+4){
       printf("write syn\n");
+      printf("syn:%lld\n",wdata);
       uint32_t data = 0;
       for (int i = 0; i < 4; i++) {
         if (wmask & 0x1) data = data | (wdata & 0xff);
@@ -246,7 +247,7 @@ extern "C" void pmem_write(long long waddr, long long wdata, char wmask) {
         data = data << 8;
       }
       vgactl_port_base[1] = data;
-      printf("syn:%d\n",data);
+      
       return;
     }
   }
