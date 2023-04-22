296572ea9a1c6bffb7dcdc64107b5d5cdefb72a1
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 11:47:41 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:47:41 up 56 min,  0 users,  load average: 1.08, 0.55, 0.34

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -506,6 +506,7 @@ void difftest_step(uint64_t pc) {
   if(is_ecall){
     printf("ecall\n");
     ref_difftest_raise_intr(csr_reg[3]);
+    return;
   }
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
