f367e9a2664d5ad04578db54b21b4e78f5856ab4
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-22 16:18:25 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-38-generic #39~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 17 21:16:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:18:25 up  5:26,  1 user,  load average: 0.34, 0.41, 0.30

--- npc/csrc/vga.c
+++ npc/csrc/vga.c
@@ -44,6 +44,7 @@ static void init_screen() {
 }
 
 static inline void update_screen() {
+  printf("update window\n");
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(uint32_t));
   SDL_RenderClear(renderer);
   SDL_RenderCopy(renderer, texture, NULL, NULL);
