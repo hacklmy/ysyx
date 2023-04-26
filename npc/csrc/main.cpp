a7ab8494c3f5274ccf24dae9c360e6dbbc55ee96
tracer-ysyx2204 <tracer@ysyx.org>
2023-04-26 22:26:17 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-40-generic #41~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 31 16:00:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:17 up  7:36,  1 user,  load average: 0.53, 0.69, 0.68

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -316,7 +316,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
   if(raddr==KBD_ADDR){
     i8042_data_port_base[0] = key_dequeue();
     *rdata = i8042_data_port_base[0];
-    if(*rdata!=0)printf("read key : %lld\n", *rdata);
+    //if(*rdata!=0)printf("read key : %lld\n", *rdata);
     return;
   }
   if(raddr<CONFIG_MBASE||raddr>(CONFIG_MBASE+CONFIG_MSIZE)){
