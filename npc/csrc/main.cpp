88f3f50ccdcd790ab4fa9a350d729b5bf7eaf9cf
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-03 21:25:35 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:25:35 up  7:08,  0 users,  load average: 0.38, 0.32, 0.38

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -730,6 +730,7 @@ void cpu_exec(int n){
       top->clock ^= 1;
       top->eval();
       printf("%lx %x\n",top->io_pc , top->io_inst);
+      printf("%d\n",sim_time);
       #ifdef CONFIG_ITRACE
     char p[1024];
     char *s = p;
