7da97ca90433607e19655a1a51d9bdb30c17bc62
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-10 19:37:54 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:37:54 up  5:07,  0 users,  load average: 0.98, 0.50, 0.37

--- npc/vsrc/EXU.v
+++ npc/vsrc/EXU.v
@@ -452,6 +452,7 @@ module EXU_AXI(
   wire [63:0] _mem_wdata_T_12 = 32'h7 == io_inst_now ? _mem_wdata_T_1 : 64'h0; // @[Mux.scala 81:58]
   wire [63:0] _mem_wdata_T_14 = 32'h26 == io_inst_now ? {{48'd0}, _mem_wdata_T_3[15:0]} : _mem_wdata_T_12; // @[Mux.scala 81:58]
   wire [63:0] _mem_wdata_T_16 = 32'h28 == io_inst_now ? {{56'd0}, _mem_wdata_T_6[7:0]} : _mem_wdata_T_14; // @[Mux.scala 81:58]
+  wire  _T_15 = ~reset; // @[EXU_AXI.scala 194:11]
   traceregs reg_trace ( // @[EXU_AXI.scala 163:27]
     .input_reg_0(reg_trace_input_reg_0),
     .input_reg_1(reg_trace_input_reg_1),
@@ -776,6 +777,17 @@ module EXU_AXI(
     `endif
         if (~reset) begin
           $fwrite(32'h80000002,"inst_store :%d inst_load:%d\n",io_inst_store,io_inst_load); // @[EXU_AXI.scala 194:11]
+        end
+    `ifdef PRINTF_COND
+      end
+    `endif
+    `endif // SYNTHESIS
+    `ifndef SYNTHESIS
+    `ifdef PRINTF_COND
+      if (`PRINTF_COND) begin
+    `endif
+        if (_T_15) begin
+          $fwrite(32'h80000002,"mem_addr:%x\n",io_Mem_addr); // @[EXU_AXI.scala 195:11]
         end
     `ifdef PRINTF_COND
       end
