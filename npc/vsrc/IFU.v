0e4539240b69dca1f5eeef061033eae7590df5b4
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-10 16:32:25 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:32:25 up  2:01,  0 users,  load average: 1.33, 0.53, 0.33

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -14,30 +14,23 @@ module IFU_AXI(
 );
 `ifdef RANDOMIZE_REG_INIT
   reg [31:0] _RAND_0;
-  reg [31:0] _RAND_1;
 `endif // RANDOMIZE_REG_INIT
   reg  inst_ready; // @[IFU_AXI.scala 18:29]
   wire  _GEN_0 = io_axi_in_rvalid & inst_ready ? 1'h0 : 1'h1; // @[IFU_AXI.scala 19:41 20:20 22:20]
-  reg [31:0] inst; // @[IFU_AXI.scala 24:23]
-  wire  _T_2 = ~reset; // @[IFU_AXI.scala 38:11]
-  assign io_inst_valid = io_axi_in_rvalid; // @[IFU_AXI.scala 37:19]
-  assign io_inst = inst; // @[IFU_AXI.scala 36:13]
-  assign io_axi_out_araddr = io_pc[31:0]; // @[IFU_AXI.scala 26:31]
-  assign io_axi_out_arvalid = io_pc_valid; // @[IFU_AXI.scala 27:24]
-  assign io_axi_out_rready = inst_ready; // @[IFU_AXI.scala 28:23]
+  wire  _T_2 = ~reset; // @[IFU_AXI.scala 36:11]
+  assign io_inst_valid = io_axi_in_rvalid; // @[IFU_AXI.scala 35:19]
+  assign io_inst = io_axi_in_rdata[31:0]; // @[IFU_AXI.scala 34:31]
+  assign io_axi_out_araddr = io_pc[31:0]; // @[IFU_AXI.scala 24:31]
+  assign io_axi_out_arvalid = io_pc_valid; // @[IFU_AXI.scala 25:24]
+  assign io_axi_out_rready = inst_ready; // @[IFU_AXI.scala 26:23]
   always @(posedge clock) begin
     inst_ready <= reset | _GEN_0; // @[IFU_AXI.scala 18:{29,29}]
-    if (reset) begin // @[IFU_AXI.scala 24:23]
-      inst <= 32'h0; // @[IFU_AXI.scala 24:23]
-    end else begin
-      inst <= io_axi_in_rdata[31:0]; // @[IFU_AXI.scala 25:10]
-    end
     `ifndef SYNTHESIS
     `ifdef PRINTF_COND
       if (`PRINTF_COND) begin
     `endif
         if (~reset) begin
-          $fwrite(32'h80000002,"inst_valid : %d pc_ready: %d \n",io_inst_valid,io_pc_valid); // @[IFU_AXI.scala 38:11]
+          $fwrite(32'h80000002,"inst_valid : %d pc_valid:%d\n",io_inst_valid,io_pc_valid); // @[IFU_AXI.scala 36:11]
         end
     `ifdef PRINTF_COND
       end
@@ -48,7 +41,7 @@ module IFU_AXI(
       if (`PRINTF_COND) begin
     `endif
         if (_T_2) begin
-          $fwrite(32'h80000002,"inst:%x\n",io_inst); // @[IFU_AXI.scala 39:11]
+          $fwrite(32'h80000002,"inst:%x\n",io_inst); // @[IFU_AXI.scala 37:11]
         end
     `ifdef PRINTF_COND
       end
@@ -93,8 +86,6 @@ initial begin
 `ifdef RANDOMIZE_REG_INIT
   _RAND_0 = {1{`RANDOM}};
   inst_ready = _RAND_0[0:0];
-  _RAND_1 = {1{`RANDOM}};
-  inst = _RAND_1[31:0];
 `endif // RANDOMIZE_REG_INIT
   `endif // RANDOMIZE
 end // initial
