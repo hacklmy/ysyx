85b792256c5fd2799beb7ffe12c53b914656ef0d
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-10 17:56:19 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:56:19 up  3:25,  0 users,  load average: 0.73, 0.32, 0.29

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -11,6 +11,7 @@ module top(
   reg [63:0] _RAND_0;
   reg [31:0] _RAND_1;
   reg [31:0] _RAND_2;
+  reg [31:0] _RAND_3;
 `endif // RANDOMIZE_REG_INIT
   wire  axi_clock; // @[top.scala 18:21]
   wire  axi_reset; // @[top.scala 18:21]
@@ -143,6 +144,7 @@ module top(
   reg [63:0] pc_now; // @[top.scala 15:25]
   reg  execute_end; // @[top.scala 17:30]
   reg  pc_valid; // @[top.scala 72:27]
+  reg  diff_step; // @[top.scala 75:28]
   AXI axi ( // @[top.scala 18:21]
     .clock(axi_clock),
     .reset(axi_reset),
@@ -287,9 +289,9 @@ module top(
   );
   assign io_inst = ifu_step_io_inst; // @[top.scala 23:13]
   assign io_pc = pc_now; // @[top.scala 16:11]
-  assign io_pc_next = exu_step_io_pc_next; // @[top.scala 77:16]
+  assign io_pc_next = exu_step_io_pc_next; // @[top.scala 79:16]
   assign io_outval = exu_step_io_res2rd; // @[top.scala 54:15]
-  assign io_step = execute_end; // @[top.scala 75:13]
+  assign io_step = diff_step; // @[top.scala 77:13]
   assign axi_clock = clock;
   assign axi_reset = reset;
   assign axi_io_axi_in_araddr = arbiter_io_axi_out_araddr; // @[top.scala 29:19]
@@ -341,7 +343,7 @@ module top(
   assign ifu_step_io_axi_in_rdata = arbiter_io_ifu_axi_out_rdata; // @[top.scala 25:24]
   assign ifu_step_io_axi_in_rvalid = arbiter_io_ifu_axi_out_rvalid; // @[top.scala 25:24]
   assign idu_step_io_inst = ~ifu_step_io_inst_valid & ~pc_valid & ~execute_end ? ifu_step_io_inst_reg : ifu_step_io_inst
-    ; // @[top.scala 79:28]
+    ; // @[top.scala 81:28]
   assign exu_step_clock = clock;
   assign exu_step_reset = reset;
   assign exu_step_io_pc = pc_now; // @[top.scala 38:20]
@@ -364,7 +366,7 @@ module top(
   always @(posedge clock) begin
     if (reset) begin // @[top.scala 15:25]
       pc_now <= 64'h80000000; // @[top.scala 15:25]
-    end else if (execute_end) begin // @[top.scala 76:18]
+    end else if (execute_end) begin // @[top.scala 78:18]
       pc_now <= exu_step_io_pc_next;
     end
     if (reset) begin // @[top.scala 17:30]
@@ -377,6 +379,11 @@ module top(
       execute_end <= ifu_step_io_inst_valid;
     end
     pc_valid <= reset | execute_end; // @[top.scala 72:{27,27} 73:14]
+    if (reset) begin // @[top.scala 75:28]
+      diff_step <= 1'h0; // @[top.scala 75:28]
+    end else begin
+      diff_step <= execute_end; // @[top.scala 76:15]
+    end
     `ifndef SYNTHESIS
     `ifdef PRINTF_COND
       if (`PRINTF_COND) begin
@@ -431,6 +438,8 @@ initial begin
   execute_end = _RAND_1[0:0];
   _RAND_2 = {1{`RANDOM}};
   pc_valid = _RAND_2[0:0];
+  _RAND_3 = {1{`RANDOM}};
+  diff_step = _RAND_3[0:0];
 `endif // RANDOMIZE_REG_INIT
   `endif // RANDOMIZE
 end // initial
