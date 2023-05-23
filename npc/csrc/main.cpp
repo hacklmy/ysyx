5ef6f502dc77f23e09d9a9f943e3c0fdf34ffb7d
tracer-ysyx2204 <tracer@ysyx.org>
2023-05-23 21:31:30 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-41-generic #42~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 18 17:40:00 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:30 up  4:14,  1 user,  load average: 0.37, 0.59, 0.67

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -292,6 +292,7 @@ extern "C" void pmem_read(long long raddr, long long *rdata) {
     if(raddr == RTC_ADDR){
       //*rdata = time_now & 0xffffffff;
       *rdata = time_now;
+      printf("read time :%lld\n",*rdata);
     }
     else if(raddr == RTC_ADDR + 4){
       *rdata = (time_now >> 32) & 0xffffffff;
