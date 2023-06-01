0b7dedd9110de2c33ca74d58ad67fb4b07bcf267
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-01 23:48:41 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:48:41 up  8:53,  1 user,  load average: 0.87, 0.51, 0.37

--- npc/vsrc/Mul.v
+++ npc/vsrc/Mul.v
@@ -11,7 +11,7 @@ module Mul(
 `ifdef RANDOMIZE_REG_INIT
   reg [63:0] _RAND_0;
   reg [31:0] _RAND_1;
-  reg [63:0] _RAND_2;
+  reg [127:0] _RAND_2;
   reg [95:0] _RAND_3;
 `endif // RANDOMIZE_REG_INIT
   wire [2:0] booth_partial_io_y; // @[Mul.scala 89:43]
@@ -20,7 +20,7 @@ module Mul(
   wire [63:0] booth_partial_io_p; // @[Mul.scala 89:43]
   reg [63:0] res; // @[Mul.scala 51:22]
   reg [1:0] state; // @[Mul.scala 53:24]
-  reg [63:0] src1; // @[Mul.scala 55:23]
+  reg [127:0] src1; // @[Mul.scala 55:23]
   reg [64:0] src2; // @[Mul.scala 56:23]
   wire [31:0] _src1_32_T_2 = io_multiplicand[31] ? 32'hffffffff : 32'h0; // @[Bitwise.scala 74:12]
   wire [63:0] _src1_32_T_4 = {_src1_32_T_2,io_multiplicand[31:0]}; // @[Cat.scala 31:58]
@@ -35,12 +35,12 @@ module Mul(
   wire [63:0] _res_T_1 = res + booth_partial_io_p; // @[Mul.scala 93:32]
   wire [63:0] _GEN_25 = {{63'd0}, booth_partial_io_c}; // @[Mul.scala 93:53]
   wire [63:0] _res_T_3 = _res_T_1 + _GEN_25; // @[Mul.scala 93:53]
-  wire [65:0] _src1_T = {src1, 2'h0}; // @[Mul.scala 95:34]
+  wire [129:0] _src1_T = {src1, 2'h0}; // @[Mul.scala 95:34]
   wire [63:0] _GEN_3 = src2 != 65'h0 ? _res_T_3 : res; // @[Mul.scala 51:22 92:33 93:25]
-  wire [65:0] _GEN_5 = src2 != 65'h0 ? _src1_T : {{2'd0}, src1}; // @[Mul.scala 55:23 92:33 95:26]
+  wire [129:0] _GEN_5 = src2 != 65'h0 ? _src1_T : {{2'd0}, src1}; // @[Mul.scala 55:23 92:33 95:26]
   wire [63:0] _GEN_14 = 2'h1 == state ? _GEN_3 : res; // @[Mul.scala 69:18 51:22]
-  wire [65:0] _GEN_16 = 2'h1 == state ? _GEN_5 : {{2'd0}, src1}; // @[Mul.scala 69:18 55:23]
-  wire [127:0] _GEN_18 = 2'h0 == state ? real_cand : {{62'd0}, _GEN_16}; // @[Mul.scala 69:18]
+  wire [129:0] _GEN_16 = 2'h1 == state ? _GEN_5 : {{2'd0}, src1}; // @[Mul.scala 69:18 55:23]
+  wire [129:0] _GEN_18 = 2'h0 == state ? {{2'd0}, real_cand} : _GEN_16; // @[Mul.scala 69:18]
   wire [63:0] _GEN_20 = 2'h0 == state ? res : _GEN_14; // @[Mul.scala 69:18 51:22]
   wire  _T_7 = state == 2'h2; // @[Mul.scala 130:21]
   wire [31:0] _GEN_27 = state == 2'h2 ? res[63:32] : 32'h0; // @[Mul.scala 130:35 147:22]
@@ -48,7 +48,7 @@ module Mul(
   wire  _GEN_31 = state == 2'h1 ? 1'h0 : _T_7; // @[Mul.scala 125:33 127:22]
   wire [31:0] _GEN_32 = state == 2'h1 ? 32'h0 : _GEN_27; // @[Mul.scala 125:33 128:22]
   wire [31:0] _GEN_33 = state == 2'h1 ? 32'h0 : _GEN_28; // @[Mul.scala 125:33 129:22]
-  wire [127:0] _GEN_30 = reset ? 128'h0 : _GEN_18; // @[Mul.scala 55:{23,23}]
+  wire [129:0] _GEN_30 = reset ? 130'h0 : _GEN_18; // @[Mul.scala 55:{23,23}]
   partial_product booth_partial ( // @[Mul.scala 89:43]
     .io_y(booth_partial_io_y),
     .io_x(booth_partial_io_x),
@@ -59,7 +59,7 @@ module Mul(
   assign io_result_hi = state == 2'h0 ? 32'h0 : _GEN_32; // @[Mul.scala 120:23 123:22]
   assign io_result_lo = state == 2'h0 ? 32'h0 : _GEN_33; // @[Mul.scala 120:23 124:22]
   assign booth_partial_io_y = src2[2:0]; // @[Mul.scala 90:43]
-  assign booth_partial_io_x = src1; // @[Mul.scala 91:36]
+  assign booth_partial_io_x = src1[63:0]; // @[Mul.scala 91:36]
   always @(posedge clock) begin
     if (reset) begin // @[Mul.scala 51:22]
       res <= 64'h0; // @[Mul.scala 51:22]
@@ -83,7 +83,7 @@ module Mul(
     end else if (2'h2 == state) begin // @[Mul.scala 69:18]
       state <= 2'h0;
     end
-    src1 <= _GEN_30[63:0]; // @[Mul.scala 55:{23,23}]
+    src1 <= _GEN_30[127:0]; // @[Mul.scala 55:{23,23}]
     if (reset) begin // @[Mul.scala 56:23]
       src2 <= 65'h0; // @[Mul.scala 56:23]
     end else if (2'h0 == state) begin // @[Mul.scala 69:18]
@@ -98,7 +98,7 @@ module Mul(
       if (`PRINTF_COND) begin
     `endif
         if (~reset) begin
-          $fwrite(32'h80000002,"src1:%x  src2:%x\n",io_multiplicand,io_multiplier); // @[Mul.scala 150:11]
+          $fwrite(32'h80000002,"src1:%x  src2:%x\n",src1,src2); // @[Mul.scala 150:11]
         end
     `ifdef PRINTF_COND
       end
@@ -145,8 +145,8 @@ initial begin
   res = _RAND_0[63:0];
   _RAND_1 = {1{`RANDOM}};
   state = _RAND_1[1:0];
-  _RAND_2 = {2{`RANDOM}};
-  src1 = _RAND_2[63:0];
+  _RAND_2 = {4{`RANDOM}};
+  src1 = _RAND_2[127:0];
   _RAND_3 = {3{`RANDOM}};
   src2 = _RAND_3[64:0];
 `endif // RANDOMIZE_REG_INIT
