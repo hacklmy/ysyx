2e99a7faad1bbaa02527ed07cf06b28735a04033
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-07 22:20:08 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:20:08 up  7:35,  0 users,  load average: 0.79, 0.49, 0.36

--- npc/obj_dir/Vtop___024root__Slow.cpp
+++ npc/obj_dir/Vtop___024root__Slow.cpp
@@ -30984,9 +30984,10 @@ void Vtop___024root___settle__TOP__3(Vtop___024root* vlSelf) {
            ? 0U : ((0x3eU == (IData)(vlSelf->top__DOT__IDU__DOT___ALUop_T_176))
                     ? 1U : (IData)(vlSelf->top__DOT__IDU__DOT__csr_index)))];
     vlSelf->top__DOT__IDU__DOT__ds_ready_go = ((~ (IData)(vlSelf->top__DOT__IDU__DOT__conflict)) 
-                                               & ((6U 
-                                                   == (IData)(vlSelf->top__DOT__IDU__DOT___ALUop_T_176)) 
-                                                  & (IData)(vlSelf->top__DOT__IFU_io_fs_to_ds_valid)));
+                                               & (((6U 
+                                                    == (IData)(vlSelf->top__DOT__IDU__DOT___ALUop_T_176)) 
+                                                   & (IData)(vlSelf->top__DOT__IFU_io_fs_to_ds_valid)) 
+                                                  & (IData)(vlSelf->top__DOT__IDU__DOT__ds_valid)));
     vlSelf->top__DOT__IDU_io_ds_to_es_valid = ((IData)(vlSelf->top__DOT__IDU__DOT__ds_valid) 
                                                & (IData)(vlSelf->top__DOT__IDU__DOT__ds_ready_go));
     vlSelf->top__DOT__IDU__DOT__ds_allowin = (1U & 
