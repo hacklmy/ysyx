021bf8a77c04caa4442ee579fb820a4e7f73472c
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-20 22:08:20 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:08:20 up  2:32,  1 user,  load average: 0.48, 0.28, 0.28

--- npc/obj_dir/Vtop___024root__4.cpp
+++ npc/obj_dir/Vtop___024root__4.cpp
@@ -149,7 +149,11 @@ VL_INLINE_OPT void Vtop___024root___combo__TOP__8(Vtop___024root* vlSelf) {
               ? (QData)((IData)(vlSelf->top__DOT__d_cache__DOT__write_back_addr))
               : ((1U == (IData)(vlSelf->top__DOT__d_cache__DOT__state))
                   ? (QData)((IData)(vlSelf->top__DOT__d_cache__DOT__write_back_addr))
-                  : vlSelf->top__DOT__d_cache__DOT___GEN_4772));
+                  : ((2U == (IData)(vlSelf->top__DOT__d_cache__DOT__state))
+                      ? (QData)((IData)(vlSelf->top__DOT__d_cache__DOT__write_back_addr))
+                      : ((3U == (IData)(vlSelf->top__DOT__d_cache__DOT__state))
+                          ? (QData)((IData)(vlSelf->top__DOT__d_cache__DOT__write_back_addr))
+                          : vlSelf->top__DOT__d_cache__DOT___GEN_4140))));
     }
     VL_EXTEND_WW(129,128, __Vtemp6388, vlSelf->top__DOT__EXU__DOT__ALU__DOT__Div__DOT___GEN_1);
     VL_EXTEND_WW(129,128, __Vtemp6391, vlSelf->top__DOT__EXU__DOT__ALU__DOT__Div__DOT___GEN_1);
