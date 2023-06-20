97f34049507e7e204b9c1e6f896dbad8f120f171
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-20 23:38:47 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:38:47 up  4:03,  1 user,  load average: 0.65, 0.55, 0.46

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -36,11 +36,11 @@ module IFU(
   wire [63:0] seq_pc = fs_pc + 64'h4; // @[IFU.scala 57:24]
   wire [63:0] pc_next = io_br_taken ? io_br_target : seq_pc; // @[IFU.scala 58:19]
   wire  fs_allowin = ~fs_valid | fs_ready_go & io_ds_allowin; // @[IFU.scala 63:29]
-  assign io_to_ds_pc = fs_pc; // @[IFU.scala 73:17]
+  assign io_to_ds_pc = fs_pc; // @[IFU.scala 75:17]
   assign io_fs_to_ds_valid = fs_valid & fs_ready_go; // @[IFU.scala 62:33]
-  assign io_inst = fs_inst; // @[IFU.scala 99:13]
-  assign io_axi_out_araddr = pc_next[31:0]; // @[IFU.scala 83:23]
-  assign io_axi_out_arvalid = io_ds_ready_go; // @[IFU.scala 84:24]
+  assign io_inst = fs_inst; // @[IFU.scala 101:13]
+  assign io_axi_out_araddr = pc_next[31:0]; // @[IFU.scala 85:23]
+  assign io_axi_out_arvalid = io_ds_ready_go; // @[IFU.scala 86:24]
   assign io_axi_out_rready = ~fs_valid | fs_ready_go & io_ds_allowin; // @[IFU.scala 63:29]
   assign io_clear_cache = io_fence & ~cache_init; // @[IFU.scala 48:32]
   always @(posedge clock) begin
@@ -61,7 +61,7 @@ module IFU(
     end
     if (reset) begin // @[IFU.scala 43:24]
       fs_pc <= 64'h7ffffffc; // @[IFU.scala 43:24]
-    end else if (fs_allowin) begin // @[IFU.scala 65:21]
+    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 68:36]
       if (io_br_taken) begin // @[IFU.scala 58:19]
         fs_pc <= io_br_target;
       end else begin
@@ -79,7 +79,7 @@ module IFU(
     `endif
         if (~reset) begin
           $fwrite(32'h80000002,"fs_pc:%x fs_valid:%d fs_allowin:%d fs_inst:%x rvalid:%d next_pc:%x\n",fs_pc,fs_valid,
-            fs_allowin,fs_inst,io_axi_in_rvalid,pc_next); // @[IFU.scala 102:11]
+            fs_allowin,fs_inst,io_axi_in_rvalid,pc_next); // @[IFU.scala 104:11]
         end
     `ifdef PRINTF_COND
       end
