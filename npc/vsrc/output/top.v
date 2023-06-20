f037a133aba70f9c34e5d2ef14a27e85f43174f3
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-20 23:43:53 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:43:53 up  4:08,  1 user,  load average: 0.80, 0.56, 0.47

--- npc/vsrc/output/top.v
+++ npc/vsrc/output/top.v
@@ -406,31 +406,31 @@ module IFU(
   reg  fs_valid; // @[IFU.scala 30:27]
   reg  fs_ready_go; // @[IFU.scala 31:30]
   reg  cache_init; // @[IFU.scala 36:29]
-  wire  fs_to_ds_valid = fs_valid & fs_ready_go; // @[IFU.scala 62:33]
+  wire  fs_to_ds_valid = fs_valid & fs_ready_go; // @[IFU.scala 59:33]
   wire  _GEN_0 = fs_to_ds_valid & io_ds_allowin & cache_init ? 1'h0 : cache_init; // @[IFU.scala 39:60 40:20 36:29]
   wire  _GEN_1 = io_cache_init | _GEN_0; // @[IFU.scala 37:24 38:20]
   reg [63:0] fs_pc; // @[IFU.scala 43:24]
   reg [31:0] fs_inst; // @[IFU.scala 44:26]
-  wire [63:0] seq_pc = fs_pc + 64'h4; // @[IFU.scala 57:24]
-  wire [63:0] pc_next = io_br_taken ? io_br_target : seq_pc; // @[IFU.scala 58:19]
-  wire  fs_allowin = ~fs_valid | fs_ready_go & io_ds_allowin; // @[IFU.scala 63:29]
-  assign io_to_ds_pc = fs_pc; // @[IFU.scala 75:17]
-  assign io_fs_to_ds_valid = fs_valid & fs_ready_go; // @[IFU.scala 62:33]
-  assign io_inst = fs_inst; // @[IFU.scala 101:13]
-  assign io_axi_out_araddr = pc_next[31:0]; // @[IFU.scala 85:23]
-  assign io_axi_out_arvalid = io_ds_ready_go; // @[IFU.scala 86:24]
-  assign io_axi_out_rready = ~fs_valid | fs_ready_go & io_ds_allowin; // @[IFU.scala 63:29]
+  wire [63:0] seq_pc = fs_pc + 64'h4; // @[IFU.scala 54:24]
+  wire [63:0] pc_next = io_br_taken ? io_br_target : seq_pc; // @[IFU.scala 55:19]
+  wire  fs_allowin = ~fs_valid | fs_ready_go & io_ds_allowin; // @[IFU.scala 60:29]
+  assign io_to_ds_pc = fs_pc; // @[IFU.scala 73:17]
+  assign io_fs_to_ds_valid = fs_valid & fs_ready_go; // @[IFU.scala 59:33]
+  assign io_inst = fs_inst; // @[IFU.scala 99:13]
+  assign io_axi_out_araddr = pc_next[31:0]; // @[IFU.scala 83:23]
+  assign io_axi_out_arvalid = io_ds_ready_go; // @[IFU.scala 84:24]
+  assign io_axi_out_rready = ~fs_valid | fs_ready_go & io_ds_allowin; // @[IFU.scala 60:29]
   assign io_clear_cache = io_fence & ~cache_init; // @[IFU.scala 48:32]
   always @(posedge clock) begin
     if (reset) begin // @[IFU.scala 30:27]
       fs_valid <= 1'h0; // @[IFU.scala 30:27]
-    end else if (fs_allowin) begin // @[IFU.scala 65:21]
-      fs_valid <= io_axi_in_rvalid; // @[IFU.scala 66:18]
+    end else if (fs_allowin) begin // @[IFU.scala 62:21]
+      fs_valid <= io_axi_in_rvalid; // @[IFU.scala 63:18]
     end
     if (reset) begin // @[IFU.scala 31:30]
       fs_ready_go <= 1'h0; // @[IFU.scala 31:30]
     end else begin
-      fs_ready_go <= fs_valid; // @[IFU.scala 61:17]
+      fs_ready_go <= fs_valid; // @[IFU.scala 58:17]
     end
     if (reset) begin // @[IFU.scala 36:29]
       cache_init <= 1'h0; // @[IFU.scala 36:29]
@@ -439,8 +439,8 @@ module IFU(
     end
     if (reset) begin // @[IFU.scala 43:24]
       fs_pc <= 64'h7ffffffc; // @[IFU.scala 43:24]
-    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 68:36]
-      if (io_br_taken) begin // @[IFU.scala 58:19]
+    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 65:36]
+      if (io_br_taken) begin // @[IFU.scala 55:19]
         fs_pc <= io_br_target;
       end else begin
         fs_pc <= seq_pc;
@@ -448,8 +448,8 @@ module IFU(
     end
     if (reset) begin // @[IFU.scala 44:26]
       fs_inst <= 32'h0; // @[IFU.scala 44:26]
-    end else if (io_axi_in_rvalid) begin // @[IFU.scala 50:27]
-      fs_inst <= io_axi_in_rdata[31:0]; // @[IFU.scala 51:17]
+    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 65:36]
+      fs_inst <= io_axi_in_rdata[31:0]; // @[IFU.scala 67:17]
     end
     `ifndef SYNTHESIS
     `ifdef PRINTF_COND
@@ -457,7 +457,7 @@ module IFU(
     `endif
         if (~reset) begin
           $fwrite(32'h80000002,"fs_pc:%x fs_valid:%d fs_allowin:%d fs_inst:%x rvalid:%d next_pc:%x\n",fs_pc,fs_valid,
-            fs_allowin,fs_inst,io_axi_in_rvalid,pc_next); // @[IFU.scala 104:11]
+            fs_allowin,fs_inst,io_axi_in_rvalid,pc_next); // @[IFU.scala 102:11]
         end
     `ifdef PRINTF_COND
       end
