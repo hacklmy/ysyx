a5530d1d9279ca663abbd496cd1c77ef409ebbdb
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-21 12:28:59 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:28:59 up  2:36,  0 users,  load average: 1.24, 0.67, 0.45

--- npc/vsrc/IFU.v
+++ npc/vsrc/IFU.v
@@ -4,6 +4,7 @@ module IFU(
   input         reset,
   input         io_ds_allowin,
   input         io_ds_ready_go,
+  input         io_ds_valid,
   input         io_br_taken,
   input  [63:0] io_br_target,
   output [63:0] io_to_ds_pc,
@@ -26,66 +27,66 @@ module IFU(
   reg [63:0] _RAND_4;
   reg [31:0] _RAND_5;
 `endif // RANDOMIZE_REG_INIT
-  reg  br_taken_cancel; // @[IFU.scala 27:34]
-  reg  fs_valid; // @[IFU.scala 30:27]
-  reg  cache_init; // @[IFU.scala 36:29]
-  wire  _T = fs_valid & io_ds_allowin; // @[IFU.scala 39:31]
-  wire  _GEN_0 = fs_valid & io_ds_allowin & cache_init ? 1'h0 : cache_init; // @[IFU.scala 39:60 40:20 36:29]
-  wire  _GEN_1 = io_cache_init | _GEN_0; // @[IFU.scala 37:24 38:20]
-  reg  br_taken; // @[IFU.scala 42:27]
-  reg [63:0] fs_pc; // @[IFU.scala 43:24]
-  reg [31:0] fs_inst; // @[IFU.scala 44:26]
-  wire  _T_2 = io_br_taken & io_ds_ready_go; // @[IFU.scala 50:21]
-  wire  _GEN_2 = br_taken_cancel & fs_valid & io_ds_allowin ? 1'h0 : br_taken_cancel; // @[IFU.scala 52:67 53:25 27:34]
-  wire  _GEN_3 = io_br_taken & io_ds_ready_go & io_axi_in_rvalid | _GEN_2; // @[IFU.scala 50:52 51:25]
-  wire  fs_allowin = ~fs_valid | _T; // @[IFU.scala 72:29]
-  wire  _GEN_4 = br_taken & io_axi_in_rvalid & fs_allowin ? 1'h0 : br_taken; // @[IFU.scala 58:52 59:18 42:27]
-  wire  _GEN_5 = _T_2 | _GEN_4; // @[IFU.scala 56:38 57:18]
-  wire [63:0] seq_pc = fs_pc + 64'h4; // @[IFU.scala 66:24]
-  wire [63:0] pc_next = br_taken ? io_br_target : seq_pc; // @[IFU.scala 67:19]
-  assign io_to_ds_pc = fs_pc; // @[IFU.scala 86:17]
-  assign io_fs_to_ds_valid = fs_valid; // @[IFU.scala 71:33]
-  assign io_inst = fs_inst; // @[IFU.scala 112:13]
-  assign io_axi_out_araddr = pc_next[31:0]; // @[IFU.scala 96:23]
-  assign io_axi_out_arvalid = io_ds_ready_go; // @[IFU.scala 97:24]
-  assign io_axi_out_rready = ~fs_valid | _T; // @[IFU.scala 72:29]
-  assign io_clear_cache = io_fence & ~cache_init; // @[IFU.scala 48:32]
+  reg  br_taken_cancel; // @[IFU.scala 28:34]
+  reg  fs_valid; // @[IFU.scala 31:27]
+  reg  cache_init; // @[IFU.scala 37:29]
+  wire  _T = fs_valid & io_ds_allowin; // @[IFU.scala 40:31]
+  wire  _GEN_0 = fs_valid & io_ds_allowin & cache_init ? 1'h0 : cache_init; // @[IFU.scala 40:60 41:20 37:29]
+  wire  _GEN_1 = io_cache_init | _GEN_0; // @[IFU.scala 38:24 39:20]
+  reg  br_taken; // @[IFU.scala 43:27]
+  reg [63:0] fs_pc; // @[IFU.scala 44:24]
+  reg [31:0] fs_inst; // @[IFU.scala 45:26]
+  wire  _T_2 = io_br_taken & io_ds_ready_go; // @[IFU.scala 51:21]
+  wire  _GEN_2 = br_taken_cancel & fs_valid & io_ds_allowin ? 1'h0 : br_taken_cancel; // @[IFU.scala 53:67 54:25 28:34]
+  wire  _GEN_3 = io_br_taken & io_ds_ready_go & io_axi_in_rvalid | _GEN_2; // @[IFU.scala 51:52 52:25]
+  wire  fs_allowin = ~fs_valid | _T; // @[IFU.scala 73:29]
+  wire  _GEN_4 = br_taken & io_axi_in_rvalid & fs_allowin ? 1'h0 : br_taken; // @[IFU.scala 59:52 60:18 43:27]
+  wire  _GEN_5 = _T_2 & io_ds_valid | _GEN_4; // @[IFU.scala 57:51 58:18]
+  wire [63:0] seq_pc = fs_pc + 64'h4; // @[IFU.scala 67:24]
+  wire [63:0] pc_next = br_taken ? io_br_target : seq_pc; // @[IFU.scala 68:19]
+  assign io_to_ds_pc = fs_pc; // @[IFU.scala 87:17]
+  assign io_fs_to_ds_valid = fs_valid; // @[IFU.scala 72:33]
+  assign io_inst = fs_inst; // @[IFU.scala 113:13]
+  assign io_axi_out_araddr = pc_next[31:0]; // @[IFU.scala 97:23]
+  assign io_axi_out_arvalid = io_ds_ready_go; // @[IFU.scala 98:24]
+  assign io_axi_out_rready = ~fs_valid | _T; // @[IFU.scala 73:29]
+  assign io_clear_cache = io_fence & ~cache_init; // @[IFU.scala 49:32]
   always @(posedge clock) begin
-    if (reset) begin // @[IFU.scala 27:34]
-      br_taken_cancel <= 1'h0; // @[IFU.scala 27:34]
+    if (reset) begin // @[IFU.scala 28:34]
+      br_taken_cancel <= 1'h0; // @[IFU.scala 28:34]
     end else begin
       br_taken_cancel <= _GEN_3;
     end
-    if (reset) begin // @[IFU.scala 30:27]
-      fs_valid <= 1'h0; // @[IFU.scala 30:27]
-    end else if (io_br_taken & ~br_taken_cancel) begin // @[IFU.scala 73:42]
-      fs_valid <= 1'h0; // @[IFU.scala 74:18]
-    end else if (fs_allowin) begin // @[IFU.scala 75:27]
-      fs_valid <= io_axi_in_rvalid; // @[IFU.scala 76:18]
+    if (reset) begin // @[IFU.scala 31:27]
+      fs_valid <= 1'h0; // @[IFU.scala 31:27]
+    end else if (io_br_taken & ~br_taken_cancel) begin // @[IFU.scala 74:42]
+      fs_valid <= 1'h0; // @[IFU.scala 75:18]
+    end else if (fs_allowin) begin // @[IFU.scala 76:27]
+      fs_valid <= io_axi_in_rvalid; // @[IFU.scala 77:18]
     end
-    if (reset) begin // @[IFU.scala 36:29]
-      cache_init <= 1'h0; // @[IFU.scala 36:29]
+    if (reset) begin // @[IFU.scala 37:29]
+      cache_init <= 1'h0; // @[IFU.scala 37:29]
     end else begin
       cache_init <= _GEN_1;
     end
-    if (reset) begin // @[IFU.scala 42:27]
-      br_taken <= 1'h0; // @[IFU.scala 42:27]
+    if (reset) begin // @[IFU.scala 43:27]
+      br_taken <= 1'h0; // @[IFU.scala 43:27]
     end else begin
       br_taken <= _GEN_5;
     end
-    if (reset) begin // @[IFU.scala 43:24]
-      fs_pc <= 64'h7ffffffc; // @[IFU.scala 43:24]
-    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 78:36]
-      if (br_taken) begin // @[IFU.scala 67:19]
+    if (reset) begin // @[IFU.scala 44:24]
+      fs_pc <= 64'h7ffffffc; // @[IFU.scala 44:24]
+    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 79:36]
+      if (br_taken) begin // @[IFU.scala 68:19]
         fs_pc <= io_br_target;
       end else begin
         fs_pc <= seq_pc;
       end
     end
-    if (reset) begin // @[IFU.scala 44:26]
-      fs_inst <= 32'h0; // @[IFU.scala 44:26]
-    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 78:36]
-      fs_inst <= io_axi_in_rdata[31:0]; // @[IFU.scala 80:17]
+    if (reset) begin // @[IFU.scala 45:26]
+      fs_inst <= 32'h0; // @[IFU.scala 45:26]
+    end else if (io_axi_in_rvalid & fs_allowin) begin // @[IFU.scala 79:36]
+      fs_inst <= io_axi_in_rdata[31:0]; // @[IFU.scala 81:17]
     end
     `ifndef SYNTHESIS
     `ifdef PRINTF_COND
@@ -95,7 +96,7 @@ module IFU(
           $fwrite(32'h80000002,
             "fs_pc:%x fs_valid:%d fs_allowin:%d fs_inst:%x arvalid:%d rvalid:%d rdata:%x next_pc:%x br_taken:%d fs_ds_valid:%d\n"
             ,fs_pc,fs_valid,fs_allowin,fs_inst,io_axi_out_arvalid,io_axi_in_rvalid,io_axi_in_rdata[31:0],pc_next,
-            br_taken,fs_valid); // @[IFU.scala 115:11]
+            br_taken,fs_valid); // @[IFU.scala 116:11]
         end
     `ifdef PRINTF_COND
       end
