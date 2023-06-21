b7429de9a13c38bcfd9e1da69d277b0ee2f9b77c
tracer-ysyx2204 <tracer@ysyx.org>
2023-06-21 13:15:27 +0800

    > sim RTL ysyx_22051085 刘鸣宇 Linux lmy-virtual-machine 5.19.0-43-generic #44~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 22 13:39:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:15:27 up  3:22,  0 users,  load average: 0.31, 0.26, 0.27

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -437,7 +437,7 @@ static int cmd_si(char *args){
 
 void print_reg(){
   int i;
-  printf("nemu_pc:%lx\n",ref_r.pc)
+  printf("nemu_pc:%lx\n",ref_r.pc);
   for (i = 0; i < 32; i++) {
     printf("gpr[%d] %s = 0x%lx\t nemu[%d] %s = 0x%lx\n", i, regs[i], cpu_gpr.gpr[i],i, regs[i], ref_r.gpr[i]);
   }
